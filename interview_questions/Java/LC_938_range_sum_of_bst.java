31f2060552613b49697c8db8b620038be9a26ba7
Tirth Bharatiya <emailtirth@gmail.com>
2020-11-09 22:58:17 +0530

    range sum of BST

---
+++ interview_questions/Java/LC_938_range_sum_of_bst.java
@@ -0,0 +1,48 @@
+/*
+
+Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
+
+Example 1:
+    Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+    Output: 32
+
+Example 2:
+    Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+    Output: 23
+ 
+Constraints:
+    The number of nodes in the tree is in the range [1, 2 * 104].
+    1 <= Node.val <= 105
+    1 <= low <= high <= 105
+    All Node.val are unique.
+
+*/
+
+class Solution {
+    int sum = 0;
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        getSum(root, low, high);
+        return sum;
+    }
+    
+    void getSum(TreeNode root, int low, int high) {
+        if(root==null) {
+            return;
+        }
+        
+        if(root.val>=low && root.val<=high) {
+            
+            sum += root.val;
+        } 
+        
+        if(root.left != null) {
+            getSum(root.left, low, high);
+        }
+        
+        if(root.right != null) {
+            getSum(root.right, low, high);
+        }
+        
+        return;
+    }
+}
