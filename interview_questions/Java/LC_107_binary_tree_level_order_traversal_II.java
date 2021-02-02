3a31c26600dd221f203345c02704e7278da51e72
tirth1 <emailtirth@gmail.com>
2021-02-02 22:21:25 +0530

    binary tree level order traversal

--- interview_questions/Java/LC_107_binary_tree_level_order_traversal_II.java
+++ interview_questions/Java/LC_107_binary_tree_level_order_traversal_II.java
@@ -18,6 +18,53 @@ return its bottom-up level order traversal as:
 */
 
 
+
+//Method 1
+class Solution {
+    int d = 0;
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        d = depth(root);
+        List<List<Integer>> lists = new ArrayList<List<Integer>>();
+        if(root==null) {
+            return lists;
+        }
+        createLevelList(root, lists, 0);
+        return lists;
+    }
+    
+    void createLevelList(TreeNode root, List<List<Integer>> lists, int levels) {
+        if(root == null) return;
+        
+        List<Integer> list = null;
+        int l_size = lists.size();
+        if(l_size == levels) {
+            list = new ArrayList<Integer>();
+            lists.add(0, list);
+        } else {
+            list = lists.get(l_size - levels - 1);
+        }
+        list.add(root.val);
+        createLevelList(root.left, lists, levels+1);
+        createLevelList(root.right, lists, levels+1);
+        
+    }
+    
+    int depth(TreeNode root) {
+        
+        if(root==null) {
+            return 0;
+        }
+        int ls = depth(root.left);
+        int rs = depth(root.right);
+        
+        return (Math.max(ls, rs)+1);
+    }
+}
+
+
+
+//Method 2
+
 class Solution {
     int d = 0;
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
