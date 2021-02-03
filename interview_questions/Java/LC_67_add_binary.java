a171147022c51cd9908015746b5283d8d71652fa
tirth1 <emailtirth@gmail.com>
2021-02-03 11:38:18 +0530

    add two binary numbers

--- interview_questions/Java/LC_67_add_binary.java
+++ interview_questions/Java/LC_67_add_binary.java
@@ -1,3 +1,24 @@
+/*
+
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+    Input: a = "11", b = "1"
+    Output: "100"
+
+Example 2:
+    Input: a = "1010", b = "1011"
+    Output: "10101"
+ 
+Constraints:
+    1 <= a.length, b.length <= 104
+    a and b consist only of '0' or '1' characters.
+    Each string does not contain leading zeros except for the zero itself.
+
+
+*/
+
+
 class Solution {
     StringBuilder sb = new StringBuilder();
     int c = 0;  //carry
@@ -9,16 +30,15 @@ class Solution {
         int l2 = s2.length();
         int i = 0;
         for(; i<s2.length(); i++) {
-            sb.insert(0, add(s1.charAt(l1-i-1), s2.charAt(l2-i-1)));
+            sb.append(add(s1.charAt(l1-i-1), s2.charAt(l2-i-1)));
         }
         for(; i<s1.length(); i++) {
-            
-            sb.insert(0, add(s1.charAt(l1-i-1), '0'));
+            sb.append(add(s1.charAt(l1-i-1), '0'));
         }
         if(c!=0) {
-            sb.insert(0, '1');
+            sb.append('1');
         }
-        return sb.toString();
+        return sb.reverse().toString();
     }
     
     char add(char c1, char c2) {
