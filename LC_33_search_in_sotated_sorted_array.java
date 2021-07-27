8ae9869e7e9ff6a66ae55f9f8ae5a6a7b025e65c
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2021-07-27 12:55:44 +0530

    search in rotated sorted array

---
+++ LC_33_search_in_sotated_sorted_array.java
@@ -0,0 +1,60 @@
+/*
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
+For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+  Input: nums = [4,5,6,7,0,1,2], target = 0
+  Output: 4
+
+Example 2:
+  Input: nums = [4,5,6,7,0,1,2], target = 3
+  Output: -1
+
+Example 3:
+  Input: nums = [1], target = 0
+  Output: -1
+
+Constraints:
+  1 <= nums.length <= 5000
+  -10^4 <= nums[i] <= 10^4
+  All values of nums are unique.
+  nums is guaranteed to be rotated at some pivot.
+  -10^4 <= target <= 10^4
+
+*/
+
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low <= high) {
+            int mid = (low+high) >> 1;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[low] <= nums[mid]) {
+                if(nums[low] <= target && nums[mid] >= target) {
+                    high = mid-1;
+                } else {
+                    low = mid+1;
+                }
+            } else {
+                if(nums[mid] <= target && nums[high] >= target) {
+                    low = mid+1;
+                } else {
+                    high = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
