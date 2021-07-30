54ef9a31e5fe0a8d1517cc72819993b964120473
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2021-07-30 11:28:19 +0530

    longest common prefix

--- LC_14_longest_common_prefix.java
+++ LC_14_longest_common_prefix.java
@@ -21,7 +21,52 @@ Constraints:
 */
 
 
+// Using Trie
+class Solution {
+    class TrieNode {
+        char val;
+        int wc;
+        TrieNode[] child;
+        
+        TrieNode(char val) {
+            this.val = val;
+            wc = 1;
+            child = new TrieNode[26];
+        }
+    }
+    int right = Integer.MAX_VALUE;
+    public void  constructTrie(String s, TrieNode root, int i) {
+        TrieNode r = root;
+        for(int j=0; j<s.length(); j++) {
+            int idx = s.charAt(j)-'a';
+            if(r.child[idx] == null) {
+                if(i==0) {
+                    r.child[idx] = new TrieNode(s.charAt(j));    
+                } else {
+                    right = Math.min(right, j);
+                    break;    
+                } 
+            } 
+            r = r.child[idx];
+        }
+    }
+    
+    public String longestCommonPrefix(String[] strs) {
+        TrieNode root = new TrieNode('1');
+        int n = strs.length;
+        
+        String minS = strs[0];
+        for(int i=0; i<n; i++) {
+            minS = strs[i].length() < minS.length() ? strs[i] : minS;
+            constructTrie(strs[i], root, i);
+        }
+        return right<minS.length() ? strs[0].substring(0,right) : minS;
+    }
+}
 
+
+
+// Method 2
 class Solution {
     public String longestCommonPrefix(String[] strs) {
        StringBuilder sb =  new StringBuilder();
