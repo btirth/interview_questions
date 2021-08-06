a735465fb622321ce056743278c13c39aea31c74
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2021-08-06 22:04:48 +0530

    find the closest element in BST

---
+++ GFG_find_the_closest_element_in_BST.java
@@ -0,0 +1,66 @@
+/*
+
+Given a BST and an integer. Find the least absolute difference between any node value of the BST and the given integer.
+
+Example 1:
+  Input:
+          10
+        /   \
+       2    11
+     /  \ 
+    1    5
+        /  \
+       3    6
+        \
+         4
+  K = 13
+  Output: 2
+  Explanation: K=13. The node that has value nearest to K is 11. so the answer is 2
+
+Example 2:
+  Input:
+        8
+      /   \
+     1     9
+      \     \
+       4    10
+      /
+     3
+  K = 9
+  Output: 0
+  Explanation: K=9. The node that has value nearest to K is 9. so the answer is 0.
+
+Your Task:
+  Your task is to complete the function minDiff() that takes the root of the BST and an integer K as its input and returns the minimum absolute difference between any node value of the BST and the integer K.
+
+Expected Time Complexity: O(Height of the BST).
+Expected Auxiliary Space: O(Height of the BST).
+
+Constraints:
+  1 <= Number of nodes <= 100000
+
+*/
+
+
+
+class Solution
+{
+    
+    static int minDiff(Node  root, int K) 
+    { 
+        int diff = Integer.MAX_VALUE;
+        
+        while(root != null) {
+            int curr_diff = Math.abs(root.data-K);
+            diff = Math.min(diff, curr_diff);
+            if(root.data > K) {
+                root = root.left;
+            } else if(root.data < K) {
+                root = root.right;
+            } else {
+                return 0;
+            }
+        }
+        return diff;
+    } 
+}
