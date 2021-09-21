d5ca531a701339d2ef918d969c57c0af9cb82752
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2021-09-21 18:37:03 +0530

    Update LC_100_same_tree.java

--- LC_100_same_tree.java
+++ LC_100_same_tree.java
@@ -71,3 +71,24 @@ class Solution {
         return q1.size() == q2.size();
     }
 }
+
+
+
+// Recursive Approach
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+        
+        if(p==null || q == null) {
+            return false;
+        }
+        
+        if(p.val != q.val) {
+            return false;
+        }
+        
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
