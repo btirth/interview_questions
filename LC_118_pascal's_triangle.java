2d1b3b114ad6ded82553451f846c10bc0947259f
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2022-01-01 22:56:09 +0530

    Create LC_118_pascal's_triangle.java

---
+++ LC_118_pascal's_triangle.java
@@ -0,0 +1,40 @@
+/*
+Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers directly above it.
+Example:
+    Input: 5
+    Output:
+[
+     [1],
+    [1,1],
+   [1,2,1],
+  [1,3,3,1],
+ [1,4,6,4,1]
+]
+*/
+
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> lists = new ArrayList<List<Integer>>();
+        if(numRows == 0) {
+            return lists;
+        }
+        List<Integer> curr = new ArrayList<Integer>();
+        curr.add(1);
+        lists.add(curr);
+        for(int i=0; i<numRows-1; i++) {
+            List<Integer> prev = curr;
+            curr = new ArrayList<Integer>();
+            curr.add(1);
+            for(int j=0; j<prev.size()-1; j++) {
+                int a = prev.get(j) + prev.get(j+1);
+                curr.add(a);
+            }
+            curr.add(1);
+            lists.add(curr);
+        }
+        
+        return lists;
+    }
+}
