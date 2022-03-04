20a463a7e98f14180bae5b2b45e610062e80d1f3
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2022-03-04 07:48:51 +0530

    Update LC_322_coin_change.java

--- LC_322_coin_change.java
+++ LC_322_coin_change.java
@@ -35,6 +35,29 @@ Constraints:
 */
 
 
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, 100001);
+        dp[0] = 0;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=1; j<=amount; j++) {
+                if(coins[i] <= j) {
+                    dp[j] = Math.min(dp[j], 1+dp[j-coins[i]]);
+                }
+            }
+        }
+        
+        return dp[amount] >= 100001 ? -1 : dp[amount];
+    }
+}
+
+
+
+
 class Solution {
    
     public int coinChange(int[] coins, int amount) {
