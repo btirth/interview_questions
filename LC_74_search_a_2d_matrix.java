7644c580677d482d842eda8c236525a6c5732a94
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2022-03-22 07:47:14 +0530

    Update LC_74_search_a_2d_matrix.java

--- LC_74_search_a_2d_matrix.java
+++ LC_74_search_a_2d_matrix.java
@@ -21,6 +21,48 @@ Constraints:
 
 */
 
+// O(m+n logn) time complexity
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int row = -1;
+        for(int i=0; i<m; i++) {
+            if(target >= matrix[i][0] && target <= matrix[i][n-1]) {
+                row = i;
+                break;
+            }
+        }
+        
+        if(row == -1) {
+            return false;
+        }
+        
+        return binarySearch(matrix, target, row);
+    }
+    
+    boolean binarySearch(int[][] matrix, int target, int row) {
+        int left = 0;
+        int right = matrix[0].length;
+        
+        while(left <= right) {
+            int mid = (left + right)/2;
+            
+            if(matrix[row][mid] == target) {
+                return true;
+            } else if(matrix[row][mid] > target) {
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+        
+        return false;
+    }
+}
+
+
 
 // O((n+m) log(n+m)) time complexity
 class Solution {
@@ -43,43 +85,6 @@ class Solution {
             return binarySearch(matrix, target, start, mid-1, row, col);
         } else {
             return binarySearch(matrix, target, mid+1, end, row, col);
-        }
-    }
-}
-
-
-
-// O(m + n logn) time complexity
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int row = -1;
-        for(int i=0; i<m; i++) {
-            if(matrix[i][0]<=target && matrix[i][n-1]>=target) {
-                row = i;
-                break;
-            }
-        }
-        if(row == -1) {
-            return false;
-        }
-        
-        return binarySearch(matrix, row, target, 0, n);
-    }
-    
-    boolean binarySearch(int[][] matrix, int row, int target, int start, int end) {
-        if(start>end) {
-            return false;
-        }
-        
-        int mid = (start+end)/2;
-        if(matrix[row][mid] == target) {
-            return true;
-        } else if(matrix[row][mid] > target) {
-            return binarySearch(matrix, row, target, start, mid-1);
-        } else {
-            return binarySearch(matrix, row, target, mid+1, end);
         }
     }
 }
