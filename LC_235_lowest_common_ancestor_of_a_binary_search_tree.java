b1aa4383048bcba593d44599e16b69261f8a602e
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2022-03-24 08:53:30 +0530

    Update LC_235_lowest_common_ancestor_of_a_binary_search_tree.java

--- LC_235_lowest_common_ancestor_of_a_binary_search_tree.java
+++ LC_235_lowest_common_ancestor_of_a_binary_search_tree.java
@@ -27,6 +27,28 @@ Constraints:
 
 */
 
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        while(root != null) {
+            if(root == p || root == q) {
+                return root;
+            }
+            if(p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else if(p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else {
+                return root;
+            }
+        }
+        
+        return root;
+    }
+}
+
+
+
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
