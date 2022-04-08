24456ade0240fae33d9684b3b3a7b619e1606dfa
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2022-04-08 21:44:28 +0530

    Update LC_703_k_th_largest_element_in_a_stream.java

--- LC_703_k_th_largest_element_in_a_stream.java
+++ LC_703_k_th_largest_element_in_a_stream.java
@@ -33,6 +33,28 @@ Constraints:
 
 */
 
+class KthLargest {
+
+    PriorityQueue<Integer> queue;
+    int k;
+    
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        queue = new PriorityQueue<>();
+        for (int num : nums) {
+            add(num);
+        }
+    }
+    
+    public int add(int val) {
+        queue.offer(val);
+        if (queue.size() > k)
+            queue.poll();
+        return queue.peek();
+    }
+}
+
+
 
 
 class KthLargest {
