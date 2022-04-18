0239259f19736c3ba90e019101d5770c891baae1
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2022-04-18 21:27:09 +0530

    Update LC_230_Kth_smallest_element_in_a_BST.java

--- LC_230_Kth_smallest_element_in_a_BST.java
+++ LC_230_Kth_smallest_element_in_a_BST.java
@@ -43,3 +43,26 @@ class Solution {
         return right;
     }
 }
+
+
+
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            
+            root = stack.pop();
+            
+            if (--k == 0) break;
+            
+            root = root.right;
+        }
+        return root.val;
+    }
+}
