c2ad7e841e68c2812f1a11d642361d734cb643db
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2022-05-04 08:19:49 +0530

    Update LC_1679_max_number_of_k_sum_pairs.java

--- LC_1679_max_number_of_k_sum_pairs.java
+++ LC_1679_max_number_of_k_sum_pairs.java
@@ -48,3 +48,30 @@ class Solution {
         return count;
     }
 }
+
+
+
+// O(n log(n)) time complexity and O(1) space complexity
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length-1;
+        int count = 0;
+        while(left < right) {
+            int val = nums[left] + nums[right];
+            
+            if(val == k) {
+                count++;
+                left++;
+                right--;
+            } else if(val < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return count;
+    }
+}
