eddea500e40591b01162c87694d1e71b3a28427c
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2022-05-05 07:54:20 +0530

    Update LC_225_implement_stack_using_queues.java

--- LC_225_implement_stack_using_queues.java
+++ LC_225_implement_stack_using_queues.java
@@ -38,37 +38,32 @@ Follow-up: Can you implement the stack using only one queue?
 
 
 class MyStack {
-
-    Queue<Integer> q = new LinkedList<>();
-    /** Initialize your data structure here. */
+    Queue<Integer> queue = new LinkedList<>();
+    int lastAddedValue = -1;
     public MyStack() {
         
     }
     
-    /** Push element x onto stack. */
     public void push(int x) {
-        q.add(x);
-        for(int i=0; i<q.size()-1; i++) {
-            q.add(q.poll());
-        }
+        queue.add(x);
+        lastAddedValue = x;
     }
     
-    /** Removes the element on top of the stack and returns that element. */
     public int pop() {
-        return q.poll();
-    }
-    
-    /** Get the top element. */
-    public int top() {
-        return q.peek();
-    }
-    
-    /** Returns whether the stack is empty. */
-    public boolean empty() {
-        if(q.size() == 0) {
-            return true;
+        for(int i=0; i<queue.size()-1; i++) {
+            int value = queue.poll();
+            queue.add(value);
+            lastAddedValue = value;
         }
-        return false;
+        return queue.poll();
+    }
+    
+    public int top() {
+        return lastAddedValue;
+    }
+    
+    public boolean empty() {
+        return queue.isEmpty();
     }
 }
 
