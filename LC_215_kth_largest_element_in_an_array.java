c77c6e1431edb4c546bda7e4d239270546c6ad43
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2022-06-22 08:59:26 +0530

    Update LC_215_kth_largest_element_in_an_array.java

--- LC_215_kth_largest_element_in_an_array.java
+++ LC_215_kth_largest_element_in_an_array.java
@@ -19,7 +19,27 @@ Constraints:
 */
 
 
+// Time Complexity O(n) and Space Complexity O(k)
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int i=0; i<k; i++) {
+            queue.add(nums[i]);
+        }
+        
+        for(int i=k; i<nums.length; i++) {
+            if(queue.peek() < nums[i]) {
+                queue.poll();
+                queue.add(nums[i]);
+            }
+        }
+        
+        return queue.peek();
+    }
+}
 
+
+// Time Complexity O(n) and Space Complexity O(n)
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         PriorityQueue<Integer> p = new PriorityQueue<>(Collections.reverseOrder());
