f0c50faf9d78ec3f86fda1be9f3cf5141da25040
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2022-10-17 23:19:19 +0530

    Delete LC_27_remove_element.py

--- interview_questions/Python/LC_27_remove_element.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def removeElement(self, nums, val):
-        j=0
-        for i in range(0,len(nums)):
-            if(nums[i]!=val):
-                nums[j] = nums[i]
-                j=j+1
-        return j
-        
