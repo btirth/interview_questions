d01c0828f0b107141315157a3e301e33830c269b
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2023-04-04 08:57:14 +0530

    Create LC_2405. Optimal Partition of String.java

---
+++ LC_2405. Optimal Partition of String.java
@@ -0,0 +1,42 @@
+/*
+
+Given a string s, partition the string into one or more substrings such that the characters in each substring are unique. 
+That is, no letter appears in a single substring more than once.
+
+Return the minimum number of substrings in such a partition.
+
+Note that each character should belong to exactly one substring in a partition.
+
+Example 1:
+  Input: s = "abacaba"
+  Output: 4
+  Explanation:
+    Two possible partitions are ("a","ba","cab","a") and ("ab","a","ca","ba").
+    It can be shown that 4 is the minimum number of substrings needed.
+
+Example 2:
+  Input: s = "ssssss"
+  Output: 6
+  Explanation: The only valid partition is ("s","s","s","s","s","s").
+ 
+Constraints:
+  1 <= s.length <= 10^5
+  s consists of only English lowercase letters.
+
+*/
+
+class Solution {
+    public int partitionString(String s) {
+        boolean[] ch = new boolean[26];
+        int count = 1;
+        
+        for(int i=0; i<s.length(); i++) {
+            if(ch[s.charAt(i)-'a']) {
+                count++;
+                ch = new boolean[26];
+            }
+            ch[s.charAt(i)-'a'] = true;
+        }
+        return count;
+    }
+}
