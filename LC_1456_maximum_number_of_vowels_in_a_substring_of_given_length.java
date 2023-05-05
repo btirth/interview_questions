0517083a818073b628ed62e0fba8017846639cd8
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2023-05-05 23:07:04 +0530

    Create LC_1456_maximum_number_of_vowels_in_a_substring_of_given_length.java

---
+++ LC_1456_maximum_number_of_vowels_in_a_substring_of_given_length.java
@@ -0,0 +1,58 @@
+/*
+
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+Example 1:
+  Input: s = "abciiidef", k = 3
+  Output: 3
+  Explanation: The substring "iii" contains 3 vowel letters.
+
+Example 2:
+  Input: s = "aeiou", k = 2
+  Output: 2
+  Explanation: Any substring of length 2 contains 2 vowels.
+
+Example 3:
+  Input: s = "leetcode", k = 3
+  Output: 2
+  Explanation: "lee", "eet" and "ode" contain 2 vowels.
+ 
+Constraints:
+  1 <= s.length <= 10^5
+  s consists of lowercase English letters.
+  1 <= k <= s.length
+
+*/
+
+class Solution {
+    public int maxVowels(String s, int k) {
+        
+        List<Character> vowels=Arrays.asList(new Character[]{'a','e','i','o','u'});
+        int i = 0;
+        int j = 0;
+        
+        int count = 0;
+        int ans = count;
+        
+        while(i<s.length()) {
+            if(vowels.contains(s.charAt(i))) {
+                count++;
+            }
+            
+            if(i-j >= k) {
+                if(vowels.contains(s.charAt(j))) {
+                    count--;
+                }
+                
+                j++;
+            }
+            
+            ans = Math.max(ans, count);
+            i++;
+        }
+        
+        return ans;
+    }
+}
