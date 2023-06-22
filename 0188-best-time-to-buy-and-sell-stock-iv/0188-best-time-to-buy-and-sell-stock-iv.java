7cbd25aead9c12a2aecd3525ba2309456f47df3f
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2023-06-22 10:13:27 +0530

    Time: 3 ms (77.34%), Space: 43.5 MB (35.03%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,32 +1,22 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-        int[][][] dp = new int[2][k+1][prices.length];
-        for(int i=0; i<2; i++) {
-            for(int j=0; j<=k; j++) {
-                Arrays.fill(dp[i][j], -1);
+        int n = prices.length;
+        int[][][] dp = new int[n+1][2][k+1];
+        
+        for(int idx=n-1; idx>=0; idx--) {
+            for(int buy=0; buy<=1; buy++) {
+                for(int count=1; count<=k; count++) {
+                    if(buy == 1) {
+                        dp[idx][buy][count] = 
+                            Math.max(dp[idx+1][0][count] - prices[idx], dp[idx+1][1][count]);
+                    } else {
+                        dp[idx][buy][count] = 
+                            Math.max(dp[idx+1][1][count-1] + prices[idx], dp[idx+1][0][count]);
+                    }
+                }
             }
         }
 
-        return buySell(prices, 0, true, dp, k);
-    }
-
-    int buySell(int[] prices, int idx, boolean buy, int[][][] dp, int count) {
-        if(idx == prices.length || count == 0) {
-            return 0;
-        }
-
-        if(dp[buy ? 0 : 1][count][idx] != -1) {
-            return dp[buy ? 0 : 1][count][idx];
-        }
-
-        if(buy) {
-            return dp[buy ? 0 : 1][count][idx] = 
-                Math.max(buySell(prices, idx+1, false, dp, count) - prices[idx], 
-                        buySell(prices, idx+1, true, dp, count));
-        } else {
-            return dp[buy ? 0 : 1][count][idx] = 
-                Math.max(buySell(prices, idx+1, true, dp, count-1) + prices[idx], 
-                        buySell(prices, idx+1, false, dp, count));
-        }
+        return dp[0][1][k];
     }
 }
