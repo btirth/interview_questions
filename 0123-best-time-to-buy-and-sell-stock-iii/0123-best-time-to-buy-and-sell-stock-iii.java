f4d591e81fb8759eae8bf2cc09c449bddd2b1eb8
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2023-06-22 10:10:49 +0530

    Time: 80 ms (37.3%), Space: 62.8 MB (31.78%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,31 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[][][] dp = new int[2][2][prices.length];
-        for(int i=0; i<2; i++) {
-            for(int j=0; j<2; j++) {
-                Arrays.fill(dp[i][j], -1);
+        int n = prices.length;
+        int[][][] dp = new int[n+1][2][3];
+        
+        for(int idx=n-1; idx>=0; idx--) {
+            for(int buy=0; buy<=1; buy++) {
+                for(int count=1; count<=2; count++) {
+                    if(buy == 1) {
+                        dp[idx][buy][count] = 
+                            Math.max(dp[idx+1][0][count] - prices[idx], dp[idx+1][1][count]);
+                    } else {
+                        dp[idx][buy][count] = 
+                            Math.max(dp[idx+1][1][count-1] + prices[idx], dp[idx+1][0][count]);
+                    }
+                }
             }
         }
 
-        return buySell(prices, 0, true, dp, 0);
+        return dp[0][1][2];
     }
-
-    int buySell(int[] prices, int idx, boolean buy, int[][][] dp, int count) {
-        if(idx == prices.length || count == 2) {
-            return 0;
-        }
-
-        if(dp[buy ? 0 : 1][count][idx] != -1) {
-            return dp[buy ? 0 : 1][count][idx];
-        }
-
-        if(buy) {
-            return dp[buy ? 0 : 1][count][idx] = 
-                Math.max(buySell(prices, idx+1, false, dp, count) - prices[idx], 
-                        buySell(prices, idx+1, true, dp, count));
-        } else {
-            return dp[buy ? 0 : 1][count][idx] = 
-                Math.max(buySell(prices, idx+1, true, dp, count+1) + prices[idx], 
-                        buySell(prices, idx+1, false, dp, count));
-        }
-    }}
+}
