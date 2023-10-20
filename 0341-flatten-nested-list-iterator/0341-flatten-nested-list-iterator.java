59c5e1cd24718a977bfea2987c1fb30412a65130
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2023-10-20 06:55:49 +0530

    Time: 2 ms (100%), Space: 44.8 MB (9.71%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.java
@@ -0,0 +1,46 @@
+
+public class NestedIterator implements Iterator<Integer> {
+
+    List<Integer> flattenList;
+    int curr;
+    public NestedIterator(List<NestedInteger> nestedList) {
+        flattenList = new ArrayList<>();
+        curr = 0;
+        
+        for(NestedInteger nestedInt: nestedList) {
+            helper(nestedInt);
+        }
+    }
+
+    @Override
+    public Integer next() {
+        return flattenList.get(curr++);
+    }
+
+    @Override
+    public boolean hasNext() {
+        if(curr < flattenList.size()) {
+            return true;
+        }
+        
+        return false;
+    }
+    
+    void helper(NestedInteger value) {
+        
+            if(value.isInteger()) {
+                flattenList.add(value.getInteger());
+            } else {
+                for(NestedInteger nestedInt: value.getList()) {
+                    helper(nestedInt);
+                }
+            }
+        
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
