32f06f4f4218a9eab145414cc5547c0d46981f88
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2024-01-06 19:15:07 -0400

    Time: 1237 ms (5.00%) | Memory: 57.2 MB (33.39%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,42 +1,29 @@
 class Solution {
-    int n;
-    int[] dp;
-    int[][] job;
 
-    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
-        n = profit.length;
-        dp = new int[n];
-        job = new int[n][3];
+    public int jobScheduling(int[] startTime, int[] endTime, int[] jobProfit) {
+        int n = jobProfit.length;
+        int[] dp = new int[n+1];
+        int[][] job = new int[n][3];
 
-        Arrays.fill(dp, -1);
         for(int i=0; i<n; i++) {
             job[i][0] = startTime[i];
             job[i][1] = endTime[i];
-            job[i][2] = profit[i];
+            job[i][2] = jobProfit[i];
         }        
 
         Arrays.sort(job, (a,b) -> Integer.compare(a[0], b[0]));
-        return dfs(0);
-    }   
 
-    int dfs(int idx) {
-        if(idx >= n) {
-            return 0;
-        }
-
-        if(dp[idx] != -1) {
-            return dp[idx];
-        }
-
-        int profit = 0;
-        
-        for(int i=idx+1; i<n; i++) {
-            if(job[i][0] >= job[idx][1]) {
-                profit = dfs(i);
-                break;
+        for(int i=n-1; i>=0; i--) {
+            int profit = 0;
+            for(int j=i+1; j<n; j++) {
+                if(job[j][0] >= job[i][1]) {
+                    profit = dp[j];
+                    break;
+                }
             }
-        }
 
-        return dp[idx] = Math.max(profit + job[idx][2], dfs(idx+1));
-    }                 
+            dp[i] = Math.max(profit+job[i][2], dp[i+1]);
+        }
+        return dp[0];
+    }   
 }
