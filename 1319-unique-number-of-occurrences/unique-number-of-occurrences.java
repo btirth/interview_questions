6c9eb259acd8e06b1567e68e3a0c8faeebeb662d
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2024-01-17 00:27:14 -0400

    Time: 7 ms (5.22%) | Memory: 42.7 MB (5.39%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        int[] freq = new int[2001];
+
+        for(int num: arr) {
+            freq[num+1000]++;
+        }
+
+        Arrays.sort(freq);
+        for(int i=1; i<2001; i++) {
+            if(freq[i] != 0 && freq[i] == freq[i-1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
