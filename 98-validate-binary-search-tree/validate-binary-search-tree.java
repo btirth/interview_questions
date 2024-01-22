f10cb96410bedf3a7e0c0ae0407878df1c9e05a7
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2024-01-22 01:16:02 -0400

    Time: 0 ms (100.00%) | Memory: 42.9 MB (94.72%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validateBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    boolean validateBST(TreeNode root, long low, long high) {
+        if(root == null) {
+            return true;
+        }
+
+        if(root.val <= low || root.val >= high) {
+            return false;
+        }
+
+        return validateBST(root.left, low, root.val) && validateBST(root.right, root.val, high);
+    }
+}
