61143a162137cffbf4acb74606e773b8a112a723
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2024-01-21 23:14:21 -0400

    Time: 4 ms (35.15%) | Memory: 45.8 MB (11.88%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int sum = (n*(n+1))/2;
         int[] ans = new int[2];
+        
         for(int i=0; i<nums.length; i++) {
             int num = Math.abs(nums[i]);
-            if(ans[0]==0 && nums[num-1] < 0) {
+            if(nums[num-1] < 0) {
                 ans[0] = num;
             } else {
+                sum -= num;
                 nums[num-1] *= -1;
             }
         }
 
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] > 0) {
-                ans[1] = i+1;
-                break;
-            }
-        }
-
+        ans[1] = sum;
         return ans;
     }
 }
