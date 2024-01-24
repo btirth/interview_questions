bcf05e4d2658fc07771d5451e49574de2ad0e2f8
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2024-01-24 01:37:21 -0400

    Time: 24 ms (39.01%) | Memory: 44.7 MB (38.11%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,22 +1,37 @@
 class Solution {
+    private int result = 0;
     public int maxLength(List<String> arr) {
-        List<Integer> dp = new ArrayList<>();
-        dp.add(0);
-        int res = 0;
-        for (String s : arr) {
-            int a = 0, dup = 0;
-            for (char c : s.toCharArray()) {
-                dup |= a & (1 << (c - 'a'));
-                a |= 1 << (c - 'a');
-            }
-            if (dup > 0)    continue;
-            for (int i = dp.size() - 1; i >= 0; i--) {
-                if ((dp.get(i) & a) > 0) continue;
-                dp.add(dp.get(i) | a);
-                res = Math.max(res, Integer.bitCount(dp.get(i) | a));
-            }
-            
+            if (arr == null || arr.size() == 0)    return 0;
+
+        dfs(arr, "", 0);
+        return result;
+    }
+
+    private void dfs(List<String> arr, String path, int idx) {
+        boolean isUniqueChar = isUniqueChars(path);
+
+        if (isUniqueChar) {
+        result = Math.max(path.length(), result);
         }
-        return res;
+
+        if (idx == arr.size() || !isUniqueChar) {
+        return;
+        }
+
+        for (int i = idx; i < arr.size(); i++) {
+        dfs(arr, path + arr.get(i), i + 1);
+        }
+
+    }
+
+    private boolean isUniqueChars(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()) {
+        if (set.contains(c)) {
+            return false;
+        }
+        set.add(c);
+        }
+        return true;
     }
 }
