03ebc6e87604142fabe8eaac516a48ac46793e1b
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2024-01-26 02:19:20 -0400

    Time: 11 ms (24.80%) | Memory: 41.8 MB (82.00%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.java
+++ 576-out-of-boundary-paths/out-of-boundary-paths.java
@@ -1,40 +1,25 @@
 class Solution {
-
-    Integer[][][] dp;
-    int MOD = (int)1e9 + 7;
-    public int findPaths(int m, int n, int mv, int startRow, int startColumn) {
-        dp = new Integer[m+1][n+1][mv+1];
-        return dfs(m, n, mv, startRow, startColumn);
+  public int findPaths(int m, int n, int N, int x, int y) {
+    int M = 1000000000 + 7;
+    int dp[][] = new int[m][n];
+    dp[x][y] = 1;
+    int count = 0;
+    for (int moves = 1; moves <= N; moves++) {
+      int[][] temp = new int[m][n];
+      for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+          if (i == m - 1) count = (count + dp[i][j]) % M;
+          if (j == n - 1) count = (count + dp[i][j]) % M;
+          if (i == 0) count = (count + dp[i][j]) % M;
+          if (j == 0) count = (count + dp[i][j]) % M;
+          temp[i][j] = (
+              ((i > 0 ? dp[i - 1][j] : 0) + (i < m - 1 ? dp[i + 1][j] : 0)) % M +
+              ((j > 0 ? dp[i][j - 1] : 0) + (j < n - 1 ? dp[i][j + 1] : 0)) % M
+          ) % M;
+        }
+      }
+      dp = temp;
     }
-
-    int[][] directions = {
-        {0,1},
-        {1,0},
-        {-1,0},
-        {0,-1}
-    };
-
-    int dfs(int m, int n, int mv, int i, int j) {
-        if(i<0 || j<0 || i==m || j==n) {
-            return 1;
-        }
-
-        if(mv == 0) {
-            return 0;
-        }
-
-        if(dp[i][j][mv] != null) {
-            return dp[i][j][mv];
-        }
-
-        int ans = 0;
-        for(int[] dir: directions) {
-            int x = i + dir[0];
-            int y = j + dir[1];
-
-            ans = (ans + dfs(m, n, mv-1, x, y))%MOD;
-        }
-
-        return dp[i][j][mv] = ans;
-    }
+    return count;
+  }
 }
