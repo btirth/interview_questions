d18d39711c04af66036236b66f4ee7b9bb195d5d
Tirth Bharatiya <41485330+tirth1@users.noreply.github.com>
2024-02-15 01:39:21 -0400

    Time: 29 ms (69.56%) | Memory: 56.1 MB (88.57%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.java
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.java
@@ -4,14 +4,12 @@ class Solution {
         int n=nums.length;
         long sum = nums[0] + nums[1];
         long ans = -1;
-        int l = 0;
+
         for(int i=2; i<n; i++) {
-            
             sum += nums[i];
-            if(i-l >= 2 && sum-nums[i] > nums[i]) {
+            if(sum-nums[i] > nums[i]) {
                 ans = Math.max(ans, sum);
-            }
-            
+            }    
         }
 
         return ans;
