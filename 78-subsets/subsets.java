27c1e3ed70ec0ec40ddc37ff59e9c4d071a40d40
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-05-01 20:48:14 -0700

    Time: 6 ms (5.31%) | Memory: 42.8 MB (17.47%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        // 8 = 1<<n = 2^n
+        for(int i=0; i<(1<<n); i++) {
+            List<Integer> subset = new ArrayList<>();
+
+            for(int j=0; j<n; j++) {
+                int bit = 1<<j;
+                if((i>>j & 1) == 1) {
+                    subset.add(nums[j]);
+                }
+            }
+
+            if(!ans.contains(subset))
+                ans.add(new ArrayList<>(subset));
+        }
+
+        return ans;
+    }
+}
