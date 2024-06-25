ac9df7d46897760c29cc6e4e88b3c8bb2924d18c
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-06-24 21:55:27 -0300

    Time: 0 ms (100.00%) | Memory: 41.1 MB (44.06%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public TreeNode bstToGst(TreeNode root) {
+        int nodeSum = 0;
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode node = root;
+
+        while (!st.empty() || node != null) {
+            while (node != null) {
+                st.push(node);
+                node = node.right;
+            }
+
+            // Store the top value of stack in node and pop it.
+            node = st.pop();
+
+            // Update value of node.
+            nodeSum += node.val;
+            node.val = nodeSum;
+
+            // Move to the left child of node.
+            node = node.left;
+        }
+        return root;
+    }
+}
