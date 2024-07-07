6d20adea455e8c1c449fd19d84d04e7574228fbb
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-07-06 22:38:53 -0300

    Time: 0 ms (100.00%) | Memory: 40.6 MB (14.30%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int count = 0;
+
+        while(numBottles >= numExchange) {
+            count += numExchange;
+            numBottles -= numExchange;
+            numBottles++;
+        }
+
+        count += numBottles;
+        return count;
+    }
+}
