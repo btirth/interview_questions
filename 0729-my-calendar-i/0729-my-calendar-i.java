13a770d04110e5e2dc90f5b42fcd17dd733289a3
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-09-25 23:22:40 -0400

    Time: 57 ms (38.94%), Space: 45.3 MB (68.72%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -0,0 +1,73 @@
+class MyCalendar {
+    class Event implements Comparable<Event> {
+        int start;
+        int end;
+
+        Event(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int compareTo(Event event) {
+            if(this.start == event.start) {
+                return Integer.compare(this.end, event.end);
+            }
+
+            return Integer.compare(this.start, event.start);
+        }
+    }
+
+    List<Event> events;
+    public MyCalendar() {
+        events = new ArrayList<>();
+    }
+    
+    public boolean book(int start, int end) {
+        if(isPossible(start, end)) {
+            events.add(new Event(start, end));
+            Collections.sort(events);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean isPossible(int start, int end) {
+        int left = 0;
+        int right = events.size()-1;
+
+        while(left <= right) {
+            int mid = (right + left)/2;
+            Event midEvent = events.get(mid);
+
+            // check overlap
+            // 3 possibilities:
+            // 1. before start and ends after start and before end
+            // 2. before start and ends after end
+            // 3. after start and ends after end
+            // 4. after start and before end
+            if(
+                (start <= midEvent.start && end > midEvent.start) ||
+                (start >= midEvent.start && start < midEvent.end) ||
+                (start >= midEvent.start && end <= midEvent.end)
+            ) {
+                return false;
+            } 
+
+            // go left
+            if(midEvent.start >= end) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
