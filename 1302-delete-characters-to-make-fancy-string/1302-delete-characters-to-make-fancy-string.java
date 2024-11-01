51fdb6cdfafdfc2538cee6a3e56cdaf57343aef0
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-10-31 23:30:11 -0400

    Time: 17 ms (100%), Space: 46.1 MB (7.14%) - LeetHub

---
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String makeFancyString(String s) {
+        char[] sChar = s.toCharArray();
+        int leftPt = 0;
+        int rightPt = 0;
+        for(int i=0; i<s.length(); i++) {
+            if(sChar[leftPt] == sChar[i]) {
+                if(i - leftPt > 1) {
+
+                } else {
+                    sChar[rightPt] = sChar[i];
+                    rightPt++;
+                }
+            } else {
+                leftPt = i;
+                sChar[rightPt] = sChar[i];
+                rightPt++;
+            }
+        }
+
+        return new String(sChar).substring(0, rightPt);
+    }
+}
