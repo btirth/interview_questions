84637c3fcefe2b0c914b4323d08e9f7cafd8964a
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-11-03 20:26:52 -0500

    Time: 17 ms (80.51%), Space: 45.7 MB (79.79%) - LeetHub

--- 3451-string-compression-iii/3451-string-compression-iii.java
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -1,18 +1,16 @@
 class Solution {
     public String compressedString(String word) {
-        char[] ch = word.toCharArray();
         StringBuilder comp = new StringBuilder("");
         int idx = 0;
         while(idx < word.length()) {
             int count = 0;
-            char currCh = ch[idx];
-            while(idx < word.length() && count < 9 && ch[idx] == currCh) {
+            char currCh = word.charAt(idx);
+            while(idx < word.length() && count < 9 && word.charAt(idx) == currCh) {
                 count++;
                 idx++;
             }
 
-            comp.append(""+count);
-            comp.append(""+currCh);
+            comp.append(count).append(currCh);
         }
 
         return comp.toString();
