4a0b0c894561bbc1754072461e544e24fa99c432
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-11-05 20:13:47 -0500

    Time: 94 ms (5.71%), Space: 45.4 MB (5.66%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean canSortArray(int[] nums) {
+        HashMap<Integer, Integer> setBitsMap = new HashMap<>();
+        for(int num: nums) {
+            setBitsMap.put(num, getSetBits(num));
+        }
+        int n = nums.length;
+        for(int i=0; i<n; i++) {
+            for(int j=1; j<n; j++) {
+                if(nums[j] < nums[j-1]) {
+                    if(setBitsMap.get(nums[j]) == setBitsMap.get(nums[j-1])) {
+                        int temp = nums[j-1];
+                        nums[j-1] = nums[j];
+                        nums[j] = temp;
+                    } 
+                }
+            }
+        }
+
+        for(int i=1; i<n; i++) {
+            if(nums[i] < nums[i-1]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    int getSetBits(int num) {
+        int setBits = 0;
+        int bit = 1;
+        for(int i=0; i<=8; i++) {
+            if((int)(num & bit) > 0) {
+                setBits++;
+            }
+
+            bit = bit << 1;
+        }
+
+        System.out.println(num+" "+setBits);
+        return setBits;
+    }
+}
