462f568ebfd41d95d78098a3410f5ddb8c5814cd
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-11-09 00:20:05 -0500

    Time: 1 ms (87.5%), Space: 40.7 MB (71.15%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.java
+++ 3394-minimum-array-end/3394-minimum-array-end.java
@@ -1,8 +1,27 @@
 class Solution {
     public long minEnd(int n, int x) {
+        /*
         long res = x;
         for(int i=1; i<n; i++) {
             res = x | (res + 1);
+        }
+
+        return res;
+        */
+
+        long res = x;
+        long x_mask = 1;
+        long n_mask = 1;
+        
+        while(n_mask <= n-1) {
+            if((x & x_mask) == 0) {
+                if((n_mask & (n-1)) > 0) {
+                    res |= x_mask;
+                }
+                n_mask = n_mask << 1;
+            }
+
+            x_mask = x_mask << 1;
         }
 
         return res;
