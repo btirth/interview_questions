07f4d104fac8f84ca348fc5f2d359619a25fddac
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-11-15 20:22:43 -0500

    Time: 8 ms (6.87%), Space: 45.5 MB (27.66%) - LeetHub

---
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] resultsArray(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n-k+1];
+        Arrays.fill(ans, -1);
+        int lastMisMatch = -1;
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        for(int i=0; i<n; i++) {
+            if(i>=k && deque.peekFirst() == nums[i-k]) {
+                System.out.println(deque.pollFirst());
+                
+            }
+
+            while(!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.pollLast();
+            }
+
+            deque.add(nums[i]);
+
+            if(i != 0 && nums[i] != nums[i-1] + 1) {
+                lastMisMatch = i-1;
+            }
+
+            if(lastMisMatch < i-k+1 && i>=k-1) {
+                ans[i-k+1] = deque.peekFirst();
+            }
+        }
+
+        return ans;
+    }
+}
