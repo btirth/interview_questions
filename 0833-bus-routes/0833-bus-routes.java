22d10a9f86d8fa865137f2271b943ae938d4f3c4
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-12-07 00:43:38 -0400

    Time: 329 ms (46.47%), Space: 70.7 MB (79.16%) - LeetHub

--- 0833-bus-routes/0833-bus-routes.java
+++ 0833-bus-routes/0833-bus-routes.java
@@ -1,53 +1,54 @@
 class Solution {
     public int numBusesToDestination(int[][] routes, int source, int target) {
-        if (source == target) {
+        int n = routes.length;
+        boolean[] visited = new boolean[n];
+        if(source == target) {
             return 0;
         }
+        HashMap<Integer, List<Integer>> stationRoutes = new HashMap<>();
 
-        HashMap<Integer, ArrayList<Integer>> adjList = new HashMap<>();
-        // Create a map from the bus stop to all the routes that include this stop.
-        for (int r = 0; r < routes.length; r++) {
-            for (int stop : routes[r]) {
-                // Add all the routes that have this stop.
-                ArrayList<Integer> route = adjList.getOrDefault(stop, new ArrayList<>());
-                route.add(r);
-                adjList.put(stop, route);
+        for(int i=0; i<n; i++) {
+            for(int station: routes[i]) {
+                List<Integer> stationRoute = new ArrayList<>();
+                if(stationRoutes.containsKey(station)) {
+                    stationRoute = stationRoutes.get(station);
+                }
+
+                stationRoute.add(i);
+                stationRoutes.put(station, stationRoute);
             }
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        Set<Integer> vis = new HashSet<Integer>(routes.length);
-        // Insert all the routes in the queue that have the source stop.
-        for (int route : adjList.get(source)) {
-            q.add(route);
-            vis.add(route);
-        }
 
-        int busCount = 1;
-        while (!q.isEmpty()) {
-            int size = q.size();
+        Queue<Integer> stationQueue = new LinkedList<>();
+        int buses = 0;
+        stationQueue.add(source);
 
-            for (int i = 0; i < size; i++) {
-                int route = q.remove();
+        while(!stationQueue.isEmpty()) {
+            int size = stationQueue.size();
+            while(size-- > 0) {
+                int station = stationQueue.poll();
+                if(!stationRoutes.containsKey(station)) {
+                    continue;
+                }
+                for(int route: stationRoutes.get(station)) {
+                    if(!visited[route]) {
+                        visited[route] = true;
 
-                // Iterate over the stops in the current route.
-                for (int stop: routes[route]) {
-                    // Return the current count if the target is found.
-                    if (stop == target) {
-                        return busCount;
-                    }
+                        for(int nextStation: routes[route]) {
+                            stationQueue.add(nextStation);
 
-                    // Iterate over the next possible routes from the current stop.
-                    for (int nextRoute : adjList.get(stop)) {
-                        if (!vis.contains(nextRoute)) {
-                            vis.add(nextRoute);
-                            q.add(nextRoute);
+                            if(nextStation == target) {
+                                return buses + 1;
+                            }
                         }
                     }
                 }
             }
-            busCount++;
+
+            buses++;
         }
+
         return -1;
     }
-};
+}
