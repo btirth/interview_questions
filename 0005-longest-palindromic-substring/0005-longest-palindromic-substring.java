ad47bf73042f7e6d03c484f5056e66fbf9896b24
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-12-11 00:41:05 -0400

    Time: 147 ms (33.87%), Space: 46.9 MB (6.62%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,26 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String ans = "" + s.charAt(0);
-        int len = 1;
         int n = s.length();
-        int[][] dp = new int[n][n];
+        boolean[][] dp = new boolean[n+1][n+1];
+        String longestPalindrome = "";
         for(int i=n-1; i>=0; i--) {
             for(int j=i; j<n; j++) {
                 if(i == j) {
-                    dp[i][j] = 1;
+                    dp[i][j] = true;
+                } else if(i+1 == j) {
+                    dp[i][j] = s.charAt(i) == s.charAt(j);
                 } else {
                     if(s.charAt(i) == s.charAt(j)) {
-                        if(i+1 == j) {
-                            dp[i][j] = 2;
-                        } else if(dp[i+1][j-1]>0) {
-                            dp[i][j] = 2 + dp[i+1][j-1];
-                        }
+                        dp[i][j] = dp[i+1][j-1];
+                    } 
+                }
 
-                        if(dp[i][j] > len) {
-                            len = dp[i][j];
-                            ans = s.substring(i, j+1);
-                        }
-                    }
+                if(dp[i][j] && j - i + 1 > longestPalindrome.length()) {
+                    longestPalindrome = s.substring(i, j+1);
                 }
             }
         }
 
-        return ans;
+        return longestPalindrome;
     }
 }
