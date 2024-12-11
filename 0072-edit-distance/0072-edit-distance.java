0f38acc81ca56727ce14c9768c6f8bc47cb311b9
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-12-11 14:19:26 -0400

    Time: 5 ms (66.56%), Space: 45.3 MB (17.63%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -25,12 +25,11 @@ class Solution {
 
         for(int i=1; i<=m; i++) {
             for(int j=1; j<=n; j++) {
-               
-                dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i][j-1]));
-                
                 if(word1.charAt(i-1) == word2.charAt(j-1)) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1]);
-                } 
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i-1][j], Math.min(dp[i-1][j-1], dp[i][j-1]));
+                }
             }
         }
 
