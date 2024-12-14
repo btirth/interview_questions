a65e031b832f04a1bd05643d28ba1d6359542220
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-12-14 00:36:09 -0400

    Time: 19 ms (8.44%), Space: 46.2 MB (5.67%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,33 +1,59 @@
 class Solution {
     public int candy(int[] ratings) {
+        /**
+        
+        [1,0,2]
+        for 0 -> [1,1,2]
+        for 1 -> [1,1,2]
+        for 2 -> [1,1,2]
+
+
+        [1,2,2]
+        for 1 -> [1,2,0]
+        for 2 -> [1,2,2]
+
+        [4,2,1,2,4] 
+        for 1 -> [0,2,1,2,0]
+        for 2 -> [3,2,1,2,3]
+
+        can I say that first give candies to children with lower candies
+        give they do not have candy give 1 to them
+        if()
+        if(ratings[neighbors] > ratings[curr])
+            give their neighbors = Math.max(existing[neighbors], 1 + curr children's)
+        
+         */
+
+
         int n = ratings.length;
         int[][] children = new int[n][2];
         int[] candies = new int[n];
 
         for(int i=0; i<n; i++) {
-            children[i][0] = i;
-            children[i][1] = ratings[i];
+            children[i][0] = ratings[i];
+            children[i][1] = i;
         }
 
-        Arrays.sort(children, (a,b) -> Integer.compare(a[1], b[1]));
-        for(int[] child: children) {
-            int idx = child[0];
-            int rating = child[1];
-            int candiesForChild = 1;
-            
-            if(idx>0 && rating > ratings[idx-1]) {
-                candiesForChild = Math.max(candiesForChild, candies[idx-1] + 1);
+        Arrays.sort(children, (a,b) -> Integer.compare(a[0], b[0]));
+        
+        for(int i=0; i<n; i++) {
+            int rating = children[i][0];
+            int idx = children[i][1];
+
+            if(candies[idx] == 0) {
+                candies[idx] = 1;
             }
 
-            if(idx < n-1 && rating > ratings[idx+1]) {
-                candiesForChild = Math.max(candiesForChild, candies[idx+1] + 1);
+            if(idx > 0 && ratings[idx-1] > ratings[idx]) {
+                candies[idx-1] = Math.max(candies[idx-1], 1 + candies[idx]);
             }
 
-            candies[idx] = candiesForChild;
+            if(idx < n-1 && ratings[idx+1] > ratings[idx]) {
+                candies[idx+1] = Math.max(candies[idx+1], 1 + candies[idx]);
+            }
         }
 
         int totalCandies = 0;
-
         for(int candy: candies) {
             totalCandies += candy;
         }
