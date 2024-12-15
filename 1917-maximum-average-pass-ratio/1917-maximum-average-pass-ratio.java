b9263229d2172b2306a8d0beabb5b9bd14bb9fde
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-12-15 14:07:34 -0400

    Time: 371 ms (54.43%), Space: 85 MB (80.49%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.java
@@ -2,10 +2,9 @@ class Solution {
     public double maxAverageRatio(int[][] classes, int extraStudents) {
         int n = classes.length;
         PriorityQueue<Pair<Double, Integer>> pq = new PriorityQueue<>((a,b) -> Double.compare(b.getKey(), a.getKey()));
+        
         for(int i=0; i<n; i++) {
-            double currRatio = ((double)classes[i][0] / (double)classes[i][1]);
-            double nextRatio = ((double)(classes[i][0]+1.0) / (double)(classes[i][1]+1.0));
-            pq.add(new Pair(nextRatio - currRatio, i));
+            pq.add(new Pair(getCurrAndNextRatioDiff(classes[i][0], classes[i][1]), i));
         }
 
         while(extraStudents-- > 0) {
@@ -13,17 +12,21 @@ class Solution {
             int idx = (int)p.getValue();
             classes[idx][0] += 1;
             classes[idx][1] += 1;
-            double currRatio = ((double)classes[idx][0] / (double)classes[idx][1]);
-            double nextRatio = ((double)(classes[idx][0]+1.0) / (double)(classes[idx][1]+1.0));
-            pq.add(new Pair(nextRatio - currRatio, idx));
+            pq.add(new Pair(getCurrAndNextRatioDiff(classes[idx][0], classes[idx][1]), idx));
         }
 
         double total = 0.0;
-
+        
         for(int i=0; i<n; i++) {
             total += ((double)classes[i][0] / (double)classes[i][1]);
         }
 
         return total/n;
+    }
+
+    double getCurrAndNextRatioDiff(int pass, int totalStudents) {
+        double currRatio = ((double)pass / (double)totalStudents);
+        double nextRatio = ((double)(pass+1.0) / (double)(totalStudents+1.0));
+        return nextRatio - currRatio;
     }
 }
