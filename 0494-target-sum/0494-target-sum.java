cabd10abf8e228b2efa739a0e1a065e9ced83a98
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-12-25 21:02:37 -0400

    Time: 14 ms (56.45%), Space: 55 MB (5.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,23 +1,27 @@
 class Solution {
     Integer[][] dp;
     public int findTargetSumWays(int[] nums, int target) {
-        dp = new Integer[nums.length][10000];
-        return helper(nums, 0, target);
+        int n = nums.length;
+        dp = new Integer[n][20000];
+        helper(nums, 0, target);
+        return dp[0][1000 + target];
     }
 
     int helper(int[] nums, int idx, int target) {
-        if(idx == nums.length && target == 0) {
-            return 1;
-        }
+        if(idx == nums.length) {
+            if(target == 0) {
+                return 1;
+            }
 
-        if(idx>=nums.length) {
             return 0;
         }
 
-        if(dp[idx][target+5000] != null) {
-            return dp[idx][target+5000];
+        if(dp[idx][1000 + target] != null) {
+            return dp[idx][1000 + target];
         }
 
-        return dp[idx][target+5000] = helper(nums, idx+1, target+nums[idx]) + helper(nums, idx+1, target-nums[idx]);
+        int negative = helper(nums, idx+1, target-nums[idx]);
+        int plus = helper(nums, idx+1, target+nums[idx]);
+        return dp[idx][1000 + target] = plus + negative;
     }
 }
