1960660fa9415a8ef68ae592876b9936a25bf9df
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2024-12-31 10:39:03 -0400

    Time: 4 ms (11.01%), Space: 41.3 MB (78.02%) - LeetHub

---
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        int n = days.length;
+        int[] dp = new int[n + 1];
+
+        for(int i=0; i<n; i++) {
+            int min = dp[i] + Math.min(costs[0], Math.min(costs[1], costs[2]));
+            
+            for(int j=i-1; j>=0; j--) {
+                if(days[i] - days[j] < 7) {
+
+                    min = Math.min(dp[j] + costs[1], min);
+                }
+
+                if(days[i] - days[j] < 30) {
+                    min = Math.min(dp[j] + costs[2], min);
+                }
+            }
+
+            dp[i + 1] = min;
+        }
+
+        return dp[n];
+    }
+}
