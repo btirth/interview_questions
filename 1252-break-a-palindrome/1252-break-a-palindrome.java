cc8c197c705f104d73032949529be28f8f4acc57
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-09 01:02:01 -0400

    Time: 0 ms (100%), Space: 41.6 MB (61.19%) - LeetHub

--- 1252-break-a-palindrome/1252-break-a-palindrome.java
+++ 1252-break-a-palindrome/1252-break-a-palindrome.java
@@ -7,48 +7,18 @@ class Solution {
             return "";
         }
         
-        boolean replaced = false;
         for(int i=0; i<len; i++) {
             if(len%2 == 1 && i == len/2) {
                 continue;
             }
 
-
-            /**
-            3 -> 1
-            5 -> 01234
-            7 -> 0123456
-            
-             */
-
             if(ch[i] > 'a') {
                 ch[i] = 'a';
-                
                 return new String(ch);
-                
             }
         }
 
-        
         ch[len - 1] = 'b';
-        
-
         return new String(ch);
-    }
-
-    boolean isPalindrom(char[] ch) {
-        int left = 0;
-        int right = ch.length - 1;
-
-        while(left < right) {
-            if(ch[left] != ch[right]) {
-                return false;
-            }
-
-            left++;
-            right--;
-        }
-
-        return true;
     }
 }
