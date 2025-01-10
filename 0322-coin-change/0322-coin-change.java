091b5f26d2644e31ba5f4073e9fa95386d1de24f
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-10 00:49:31 -0400

    Time: 11 ms (95.77%), Space: 44.3 MB (76.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,14 +1,32 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp, 100000);
+        /**
+        [1,2,5] = target 11
+
+        [1,2,3,4,5,6,....,11]
+        [1,1,(3, 1+1), (4, 1+1)]
+
+        So we only need 1D DP    
+         */
+
+
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
+        Arrays.sort(coins);
         for(int coin: coins) {
-            for(int i=coin; i<=amount; i++) {
-                dp[i] = Math.min(dp[i], 1 + dp[i-coin]);
+            if(coin > amount) {
+                continue;
+            }
+            
+            dp[coin] = 1;
+            for(int i=coin + 1; i<=amount; i++) {
+                if(dp[i - coin] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
             }
         }
 
-        return dp[amount] == 100000 ? -1 : dp[amount];
+
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount]; 
     }
 }
