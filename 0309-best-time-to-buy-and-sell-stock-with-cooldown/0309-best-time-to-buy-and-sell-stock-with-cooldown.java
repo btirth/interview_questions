5abbbd560f2a8c3a6a07997ad87e36a60ca03c6d
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-10 22:04:24 -0400

    Time: 1 ms (81.73%), Space: 42.3 MB (18.27%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,55 +1,25 @@
 class Solution {
+    Integer[][] dp;
     public int maxProfit(int[] prices) {
-        /**
-        [1,2,3,0,2]
-
-        let's say DP
-        I say I buy share at 0th day and sell i th day
-        so my sub problem is maxProfix from i+2 th day
-        
-        Recursive calls -> 
-        O(n^2)
-
-
-        Now can we convert it into iterative approach ? 
-         */
-        int n = prices.length;
-        int[] dp = new int[n];
-        int maxProfit = 0;
-        for(int i=n-1; i>=0; i--) {
-            for(int j=i+1; j<n; j++) {
-                dp[i] = Math.max(dp[i], dp[j]);
-                if(j+2 < n) {
-                    dp[i] = Math.max(dp[i], prices[j] - prices[i] + dp[j + 2]);
-                } else {
-                    dp[i] = Math.max(dp[i], prices[j] - prices[i]);
-                }
-            }
-
-            maxProfit = Math.max(maxProfit, dp[i]);
-        }
-
-        return maxProfit; 
+        dp = new Integer[prices.length][2];
+        return helper(prices, 0, 1);
     }
 
+    int helper(int[] prices, int idx, int buy) {
+        if(idx >= prices.length) {
+            return 0;
+        }
 
-    // Apporach 2: Recursive 
-    // int maxProfit(int[] prices, int startDay) {
-    //     if(startDay >= n) {
-    //         return 0;
-    //     }
+        if(dp[idx][buy] != null) {
+            return (int) dp[idx][buy];
+        }
 
-    //     if(dp[startDay] != Integer.MIN_VALUE) {
-    //         return dp[startDay];
-    //     }
+        int cooldown = helper(prices, idx + 1, buy);
 
-    //     int profit = 0;
-    //     for(int i=startDay + 1; i<n; i++) {
-
-    //         profit = Math.max(profit, prices[i] - prices[startDay] + maxProfit(prices, i + 2));
-    //         profit = Math.max(profit, maxProfit(prices, i));
-    //     }
-
-    //     return dp[startDay] = profit;
-    // }
+        if(buy == 1) {
+            return dp[idx][buy] = Math.max(cooldown, helper(prices, idx + 1, 0) - prices[idx]);
+        } else {
+            return dp[idx][buy] = Math.max(cooldown, prices[idx] + helper(prices, idx + 2, 1));
+        }
+    }
 }
