6e8662ced2c88d409b8ddd0c81d5d2179ee6f105
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-11 11:34:51 -0400

    Time: 4 ms (92.86%), Space: 41.4 MB (80.17%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,13 +1,30 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount+1];
-        dp[0] = 1;
+        /**
+        [1,2,5] -> 5
 
-        for(int i=0; i<coins.length; i++) {
-            for(int j=coins[i]; j<=amount; j++) {
-                dp[j] += dp[j-coins[i]];
+        Backtracking n^(amount/min(coins))
+        0 1 2 3 4 5
+        0 1 1 1 1 1
+        0 1 2 2 3 3
+
+
+        4 -> 1111, 112, 22
+        5 -> 11111, 1112, 122
+    
+         */
+
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for(int coin: coins) {
+            if(coin > amount) {
+                continue;
             }
-        }
+
+            for(int i=coin; i<=amount; i++) {
+                dp[i] += dp[i-coin];
+            }
+        } 
 
         return dp[amount];
     }
