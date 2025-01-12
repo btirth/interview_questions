880f789a95b76477ce5f96e5ee33db1ba3d208d9
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-12 18:54:03 -0400

    Time: 2 ms (40.4%), Space: 45.1 MB (36.95%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,45 +1,72 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<Integer[]> ans = new ArrayList<>();
-        boolean added = false;
+        List<Integer[]> finalIntervals = new ArrayList<>();
+        int n = intervals.length;
+        if(n == 0) {
+            return new int[][]{{newInterval[0], newInterval[1]}};
+        }
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        boolean isAdded = false;
+        for(int i=1; i<n; i++) {
 
-        for(int[] interval: intervals) {
-            int start = interval[0];
-            int end = interval[1];
-
-            // case in which new interval will get inserted.
-            if(!added) {
-                if(newInterval[0] >= start && newInterval[0] <= end) {
+            if(!isAdded && newInterval[0] <= start) {
+                if(newInterval[1] < start) {
+                    finalIntervals.add(new Integer[]{newInterval[0], newInterval[1]});
+                } else {
+                    start = Math.min(start, newInterval[0]);
                     end = Math.max(end, newInterval[1]);
-                    added = true;
-                } else if(start >= newInterval[0]) {
-                    ans.add(new Integer[]{newInterval[0], newInterval[1]});
-                    added = true;
                 }
+                isAdded = true;
             }
-            
-            if(added && ans.size()>0) {
-                Integer[] prev = ans.get(ans.size()-1);
-                if(start <= prev[1]) {
-                    ans.remove(ans.size()-1);
-                    start = Math.min(start, prev[0]);
-                    end = Math.max(prev[1], end);
-                }
+
+            if(!isAdded && newInterval[0] <= end) {
+                end = Math.max(end, newInterval[1]);
+                isAdded = true;
             }
-        
-            ans.add(new Integer[]{start, end});
+
+            if(intervals[i][0] <= end) {
+                end = Math.max(end, intervals[i][1]);
+            } else {
+                finalIntervals.add(new Integer[]{start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
         }
 
-        if(!added) {
-            ans.add(new Integer[]{newInterval[0], newInterval[1]});
+
+        if(!isAdded && newInterval[0] <= start) {
+            if(newInterval[1] < start) {
+                finalIntervals.add(new Integer[]{newInterval[0], newInterval[1]});
+            } else {
+                start = Math.min(start, newInterval[0]);
+                end = Math.max(end, newInterval[1]);
+            }
+            isAdded = true;
         }
 
-        int[][] arr = new int[ans.size()][2];
-        for(int i=0; i<ans.size(); i++) {
-            Integer[] a = ans.get(i);
-            arr[i] = new int[]{a[0], a[1]};
+        if(!isAdded && newInterval[0] <= end) {
+            end = Math.max(end, newInterval[1]);
+            isAdded = true;
         }
 
-        return arr;
+        finalIntervals.add(new Integer[]{start, end});
+
+        if(!isAdded) {
+            finalIntervals.add(new Integer[]{newInterval[0], newInterval[1]});
+        }
+
+        // Convert into array
+        int totalIntervals = finalIntervals.size();
+        int[][] ans = new int[totalIntervals][2];
+        int idx = 0;
+        for(Integer[] interval: finalIntervals) {
+            ans[idx][0] = (int)interval[0];
+            ans[idx][1] = (int)interval[1];
+
+            idx++;
+        }
+
+        return ans;
     }
 }
