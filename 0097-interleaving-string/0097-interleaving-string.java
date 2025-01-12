1d534676f1f024adaf4fbe29181301a6d0618246
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-11 20:10:00 -0400

    Time: 1 ms (85.97%), Space: 44.8 MB (10.49%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,27 +1,37 @@
 class Solution {
+    Boolean[][] dp;
     public boolean isInterleave(String s1, String s2, String s3) {
-        int m = s1.length();
-        int n = s2.length();
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
 
-        if(m + n != s3.length()) {
+        if(len1 + len2 != len3) {
             return false;
         }
 
-        boolean[][] dp = new boolean[m+1][n+1];
-        dp[m][n] = true;
+        dp = new Boolean[101][101];
 
-        for(int i=m; i>=0; i--) {
-            for(int j=n; j>=0; j--) {
-                if(i<m && s1.charAt(i) == s3.charAt(i + j) && dp[i+1][j]) {
-                    dp[i][j] = true;
-                }
+        return helper(s1, s2, s3, 0, 0);
+    }
 
-                if(j<n && s2.charAt(j) == s3.charAt(i + j) && dp[i][j+1]) {
-                    dp[i][j] = true;
-                }
-            }
+
+    boolean helper(String s1, String s2, String s3, int idx1, int idx2) {
+        if(idx1 + idx2 == s3.length()) {
+            return true;
         }
 
-        return dp[0][0];
+        if(dp[idx1][idx2] != null) {
+            return dp[idx1][idx2];
+        }
+
+        if(idx1 < s1.length() && s1.charAt(idx1) == s3.charAt(idx1 + idx2) && helper(s1, s2, s3, idx1+1, idx2)) {
+            return dp[idx1][idx2] = true;
+        }
+
+        if(idx2 < s2.length() && s2.charAt(idx2) == s3.charAt(idx1 + idx2) && helper(s1, s2, s3, idx1, idx2+1)) {
+            return dp[idx1][idx2] = true;
+        }
+
+        return dp[idx1][idx2] = false;
     }
 }
