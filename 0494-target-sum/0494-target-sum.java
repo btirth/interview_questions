465b4c9a2ea0a0ce80f772715eabf8880ff99995
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-12 22:17:10 -0400

    Time: 7 ms (81.86%), Space: 44.8 MB (26.81%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,20 @@
 class Solution {
+    int n;
     Integer[][] dp;
+    int maxSum;
     public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
-        dp = new Integer[n][20000];
-        helper(nums, 0, target);
-        return dp[0][1000 + target];
+        n = nums.length;
+        maxSum = 0;
+        for(int num: nums) {
+            maxSum += num;
+        }
+
+        dp = new Integer[n][(2*maxSum) + 1];
+        return helper(nums, target, 0);
     }
 
-    int helper(int[] nums, int idx, int target) {
-        if(idx == nums.length) {
+    int helper(int[] nums, int target, int idx) {
+        if(idx == n || target < -1*maxSum || target > maxSum) {
             if(target == 0) {
                 return 1;
             }
@@ -16,12 +22,12 @@ class Solution {
             return 0;
         }
 
-        if(dp[idx][1000 + target] != null) {
-            return dp[idx][1000 + target];
+        if(dp[idx][target + maxSum] != null) {
+            return dp[idx][target + maxSum];
         }
 
-        int negative = helper(nums, idx+1, target-nums[idx]);
-        int plus = helper(nums, idx+1, target+nums[idx]);
-        return dp[idx][1000 + target] = plus + negative;
+        return dp[idx][target + maxSum] = helper(nums, target - nums[idx], idx+1) + 
+                                    helper(nums, target + nums[idx], idx+1);
+        
     }
 }
