d907e750a906b9286f7ae52188cd25c1e0733dbc
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-12 21:13:47 -0400

    Time: 10 ms (82.12%), Space: 45.9 MB (89.4%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -22,9 +22,7 @@ class Solution {
         for(int i=0; i<26; i++) {
             if(freq[i] == 0) {
                 continue;
-            }
-            
-            if(freq[i] % 2 == 0) {
+            } else if(freq[i] % 2 == 0) {
                 length += 2;
             } else {
                 length += 1;
