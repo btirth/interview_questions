69615857d753723d9521232d62916307d5237535
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-14 17:14:36 -0400

    Time: 109 ms (30.91%), Space: 57.8 MB (32.54%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,23 +1,49 @@
-import java.util.Stack;
-
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        Stack<Integer> stack = new Stack<>();
-        int maxArea = 0;
         int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int idx = 0;
+        Stack<Integer> stack = new Stack<>();
 
-        for (int i = 0; i <= n; i++) {
-            int currentHeight = (i == n) ? 0 : heights[i];
-
-            while (!stack.isEmpty() && heights[stack.peek()] > currentHeight) {
-                int height = heights[stack.pop()];
-                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
-                maxArea = Math.max(maxArea, height * width);
+        while(idx < n) {
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[idx]) {
+                stack.pop();
             }
 
-            stack.push(i);
+            if(stack.isEmpty()) {
+                left[idx] = -1;
+            } else {
+                left[idx] = stack.peek();
+            }
+
+            stack.push(idx);
+            idx++;
         }
 
-        return maxArea;
+        stack = new Stack<>();
+        idx = n-1;
+
+        while(idx >= 0) {
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[idx]) {
+                stack.pop();
+            }
+
+            if(stack.isEmpty()) {
+                right[idx] = n;
+            } else {
+                right[idx] = stack.peek();
+            }
+
+            stack.push(idx);
+            idx--;
+        }
+
+        int ans = 0;
+        for(int i=0; i<n; i++) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+
+        return ans;
     }
 }
