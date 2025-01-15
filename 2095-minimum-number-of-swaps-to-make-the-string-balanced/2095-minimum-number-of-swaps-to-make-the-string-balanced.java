715b2bf07c8489d2bcdc0cb620075c56fd518c83
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-15 19:51:43 -0400

    Time: 14 ms (79.95%), Space: 56.2 MB (12.9%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,17 +1,21 @@
 class Solution {
-
     public int minSwaps(String s) {
-        int stackSize = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            // If character is opening bracket, increment the stack size.
-            if (ch == '[') stackSize++;
-            else {
-                // If the character is closing bracket, and we have an opening bracket, decrease
-                // the stack size.
-                if (stackSize > 0) stackSize--;
+        int count = 0;
+        int replace = 0;
+        for(char ch: s.toCharArray()) {
+            if(ch == '[') {
+                count++;
+            } else {
+                if(count > 0) {
+                    count--;
+                } else {
+                    count++;
+                    replace++;
+                }
+                    
             }
         }
-        return (stackSize + 1) / 2;
+
+        return replace;
     }
 }
