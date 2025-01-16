082db2465c451a64284bf51b4b172abd7c0fb9fe
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-16 00:42:59 -0400

    Time: 0 ms (100%), Space: 41.6 MB (81.63%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,34 +1,51 @@
 class Solution {
-    int[] dp;
     public int numDecodings(String s) {
-        dp = new int[s.length()];
-        Arrays.fill(dp, -1);
-        return helper(s, 0);
-    }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
 
-    int helper(String s, int idx) {
-        if(s.length() == idx) {
-            return 1;
-        }
+        for(int i=n-1; i>=0; i--) {
+            if(s.charAt(i) == '0') {
+                dp[i] = 0;
+                continue;
+            }
 
-        if(s.charAt(idx) == '0') {
-            return 0;
-        }
+            dp[i] += dp[i + 1];
 
-        if(dp[idx] != -1) {
-            return dp[idx];
-        }
-
-        int val = 0;
-        val += helper(s, idx + 1);
-
-        if(idx < s.length() - 1 && (s.charAt(idx) == '1' || (s.charAt(idx) == '2' && s.charAt(idx+1) < '7'))) {
+             if(i < s.length() - 1 && 
+             (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) < '7'))) {
            
-                val += helper(s, idx + 2);
+                dp[i] += dp[i + 2];
          
+            }
         }
 
-        return dp[idx] = val;
+        return dp[0];
     }
+
+    // int helper(String s, int idx) {
+    //     if(s.length() == idx) {
+    //         return 1;
+    //     }
+
+    //     if(s.charAt(idx) == '0') {
+    //         return 0;
+    //     }
+
+    //     if(dp[idx] != -1) {
+    //         return dp[idx];
+    //     }
+
+    //     int val = 0;
+    //     val += helper(s, idx + 1);
+
+    //     if(idx < s.length() - 1 && (s.charAt(idx) == '1' || (s.charAt(idx) == '2' && s.charAt(idx+1) < '7'))) {
+           
+    //             val += helper(s, idx + 2);
+         
+    //     }
+
+    //     return dp[idx] = val;
+    // }
 
 }
