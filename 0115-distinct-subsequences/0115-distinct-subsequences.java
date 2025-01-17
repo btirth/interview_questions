dc0b18c18a39ddac6cfe574e058664abce2ad25e
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-17 19:35:30 -0400

    Time: 19 ms (61.46%), Space: 51.4 MB (39.41%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,28 +1,40 @@
 class Solution {
-    Integer[][] dp;
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
-        dp = new Integer[m][n];
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0; i<=m; i++) {
+            dp[i][n] = 1;
+        }
 
-        return helper(s, t, 0, 0);
+        for(int i=m-1; i>=0; i--) {
+            for(int j=n-1; j>=0; j--) {
+                dp[i][j] = dp[i+1][j];
+
+                if(s.charAt(i) == t.charAt(j)) {
+                    dp[i][j] += dp[i+1][j+1];
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 
-    int helper(String s, String t, int idx1, int idx2) {
-        if(idx2 == t.length()) {
-            return 1;
-        } else if(idx1 == s.length()) {
-            return 0;
-        }
+    // int helper(String s, String t, int idx1, int idx2) {
+    //     if(idx2 == t.length()) {
+    //         return 1;
+    //     } else if(idx1 == s.length()) {
+    //         return 0;
+    //     }
 
-        if(dp[idx1][idx2] != null) {
-            return dp[idx1][idx2];
-        }
-        int val = helper(s, t, idx1+1, idx2);
-        if(s.charAt(idx1) == t.charAt(idx2)) {
-            val += helper(s, t, idx1+1, idx2+1);
-        }
+    //     if(dp[idx1][idx2] != null) {
+    //         return dp[idx1][idx2];
+    //     }
+    //     int val = helper(s, t, idx1+1, idx2);
+    //     if(s.charAt(idx1) == t.charAt(idx2)) {
+    //         val += helper(s, t, idx1+1, idx2+1);
+    //     }
 
-        return dp[idx1][idx2] = val;
-    } 
+    //     return dp[idx1][idx2] = val;
+    // } 
 }
