8d032b88fecdde50a881faf452553a965c99b873
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-16 21:23:44 -0400

    Time: 2 ms (100%), Space: 62.6 MB (26.47%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -1,19 +1,12 @@
 class Solution {
     public boolean doesValidArrayExist(int[] derived) {
-        return helper(derived, 1) || helper(derived, 0);
-    }
-
-    boolean helper(int[] derived, int prev) {
+        int xor = derived[0];
         int n = derived.length;
-        int original = prev;
-        for(int i=0; i<n-1; i++) {
-            
-            if(derived[i] == 1) {
-                prev = prev == 1 ? 0 : 1;
-            } 
-            
+
+        for(int i=1; i<n; i++) {
+            xor ^= derived[i];
         }
 
-        return derived[n-1] == (prev ^ original);
+        return xor == 0;
     }
 }
