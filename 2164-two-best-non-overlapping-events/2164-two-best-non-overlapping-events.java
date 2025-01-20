7d59d4464cf1f8a37bce4ad1d71f9ea22b824aa3
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-01-20 19:06:48 -0400

    Time: 59 ms (26.27%), Space: 112.3 MB (29.62%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.java
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.java
@@ -2,34 +2,34 @@ class Solution {
     public int maxTwoEvents(int[][] events) {
         int n = events.length;
         int[][] start = new int[n][2];
-        int[][] endVal = new int[n][2];
+        int[][] end = new int[n][2];
 
         for(int i=0; i<n; i++) {
             start[i][0] = events[i][0];
             start[i][1] = events[i][2];
-            endVal[i][0] = events[i][1];
-            endVal[i][1] = events[i][2];
+            end[i][0] = events[i][1];
+            end[i][1] = events[i][2];
         }
 
         Arrays.sort(start, (a,b) -> Integer.compare(a[0], b[0]));
-        Arrays.sort(endVal, (a,b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(end, (a,b) -> Integer.compare(a[0], b[0]));
 
-        int maxVal = 0;
-        int idx = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0; i<n; i++) {
-            while(idx<n && endVal[idx][0] < start[i][0]) {
-                pq.add(endVal[idx][1]);
-                idx++;
+        int startIdx = 0;
+        int endIdx = 0;
+
+        int maxValue = 0;
+        int ans = 0;
+
+        while(startIdx < n) {
+            while(endIdx < n & end[endIdx][0] < start[startIdx][0]) {
+                maxValue = Math.max(maxValue, end[endIdx][1]);
+                endIdx++;
             }
 
-            if(!pq.isEmpty()) {
-                maxVal = Math.max(maxVal, start[i][1] + pq.peek());
-            }
-
-            maxVal = Math.max(maxVal, start[i][1]);
+            ans = Math.max(ans, maxValue + start[startIdx][1]);
+            startIdx++;
         }
 
-        return maxVal;
+        return ans;
     }
 }
