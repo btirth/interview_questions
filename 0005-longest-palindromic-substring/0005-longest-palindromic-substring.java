204efbc5b2acff78fabad39b78d0c30e692b724e
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-02-04 17:23:35 -0400

    Time: 21 ms (50.02%), Space: 44.5 MB (57.81%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,42 +1,39 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int len = s.length();
-        int resStart = 0;
-        int resLen = 1;
+        int[] startEnd = new int[2];
+        int[] ansStartEnd = new int[2];
+        int n = s.length();
 
-        for(int i=0; i<len; i++) {
-            // odd Length
-            int left = i-1;
-            int right = i+1;
-
-            while(left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
-                if(right - left + 1 > resLen) {
-                    resStart = left;
-                    resLen = right - left + 1;
-                }
-
-                left--;
-                right++;
+        for(int i=0; i<n; i++) {
+            startEnd = helper(s, i-1, i+1);
+            if(startEnd[1] - startEnd[0] > ansStartEnd[1] - ansStartEnd[0]) {
+                ansStartEnd = startEnd;
             }
-
-            
-
-
-            // even length
-            left = i;
-            right = i + 1;
-
-            while(left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
-                if(right - left + 1 > resLen) {
-                    resStart = left;
-                    resLen = right - left + 1;
-                }
-                
-                left--;
-                right++;
+            startEnd = helper(s, i, i+1);
+            if(startEnd[1] - startEnd[0] > ansStartEnd[1] - ansStartEnd[0]) {
+                ansStartEnd = startEnd;
+            }
+            startEnd = helper(s, i-1, i);
+            if(startEnd[1] - startEnd[0] > ansStartEnd[1] - ansStartEnd[0]) {
+                ansStartEnd = startEnd;
             }
         }
 
-        return s.substring(resStart, resStart + resLen);
+        return s.substring(ansStartEnd[0], ansStartEnd[1]);
+    }
+
+    int[] helper(String s, int left, int right) {
+        int n = s.length();
+
+        while(left >=0 && right < n) {
+            if(s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+            } else {
+                break;
+            }
+        }
+
+        return new int[]{left + 1, right};
     }
 }
