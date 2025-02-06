e21d50e6669728c37108a162f9e39c15b1e68977
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-02-06 00:17:24 -0400

    Time: 14 ms (35.84%), Space: 44.4 MB (71.53%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -1,42 +1,45 @@
 class Solution {
+    boolean[][] visited;
     public int swimInWater(int[][] grid) {
-        /**
-        the task is to find path from (0, 0) to (n-1, n-1) with minimum value of maximum value in the path.
-         */
-
         int m = grid.length;
         int n = grid[0].length;
-        int[][] directions = new int[][] {
+        boolean[][] visited = new boolean[m][n];
+        int ans = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[0], b[0]));
+        int[][] directions = {
             {0,1},
             {1,0},
             {-1,0},
             {0,-1}
         };
 
-        boolean[][] visited = new boolean[m][n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(grid[a[0]][a[1]], grid[b[0]][b[1]]));
-
-        pq.add(new int[]{0,0});
-        int ans = grid[0][0];
+        pq.add(new int[]{grid[0][0], 0, 0});
 
         while(!pq.isEmpty()) {
             int[] pt = pq.poll();
-            ans = Math.max(ans, grid[pt[0]][pt[1]]);
+            ans = Math.max(ans, pt[0]);
+            int i = pt[1];
+            int j = pt[2];
+            
+            if(visited[i][j]) {
+                continue;
+            }
 
-            if(pt[0] == m-1 && pt[1] == n-1) {
+            visited[i][j] = true;
+
+            if(i == m-1 && j == n-1) {
                 return ans;
             }
 
-            visited[pt[0]][pt[1]] = true;
             for(int[] dir: directions) {
-                int x = pt[0] + dir[0];
-                int y = pt[1] + dir[1];
+                int x = i + dir[0];
+                int y = j + dir[1];
 
-                if(x>=0 && y>=0 && x < m && y < n && !visited[x][y]) {
-                    pq.add(new int[]{x,y});
+                if(x>=0 && y>=0 && x<m && y<n && !visited[x][y]) {
+                    pq.add(new int[]{grid[x][y], x, y});
                 }
             }
-        } 
+        }
 
         return -1;
     }
