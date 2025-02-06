0ae4ca7da0c764e148021c9d3f7bb7b041a760b9
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-02-06 01:17:33 -0400

    Time: 6 ms (28.59%), Space: 44.6 MB (29%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,28 +1,18 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[] prev = {0};
-        int ans = Integer.MAX_VALUE;
-        for(List<Integer> list: triangle) {
-            ans = Integer.MAX_VALUE;
-            int[] curr = new int[list.size()];
-        
-            for(int i=0; i<list.size(); i++) {
-                int min;
-                if(i == 0) {
-                    min = prev[i];
-                } else if(i == list.size()-1) {
-                    min = prev[i-1];
-                } else {
-                    min = Math.min(prev[i], prev[i-1]);
-                }
-                int val = list.get(i) + min;
-                ans = Math.min(ans, val);
-                curr[i] = val;
+        int size = triangle.size();
+        List<Integer> prev = triangle.get(size - 1);
+
+        for(int i=size-2; i>=0; i--) {
+            List<Integer> curr = triangle.get(i);
+
+            for(int j=0; j<curr.size(); j++) {
+                curr.set(j, curr.get(j) + Math.min(prev.get(j), prev.get(j + 1)));
             }
 
             prev = curr;
         }
 
-        return ans;
+        return prev.get(0);
     }
 }
