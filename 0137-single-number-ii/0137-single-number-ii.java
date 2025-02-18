46d8b0e9a08a140b80e8a3ffd7207ac257b5203c
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-02-17 23:47:27 -0400

    Time: 4 ms (53.59%), Space: 45.2 MB (89.21%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,15 +1,28 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        /**
+        2 ^ 2 ^ 3 ^ 2
+
+        010
+        010
+        010
+        110
+         */
+
         int ans = 0;
         for(int i=0; i<32; i++) {
             int count = 0;
             for(int num: nums) {
-                count += (num >> i & 1);
+                if((num & (1 << i)) != 0) {
+                    count++;
+                }
             }
 
-            count %=3;
-            ans |= count << i;
-        }
+            int rem = count % 3;
+            if(rem > 0) {
+                ans |= (1 << i);
+            }
+        } 
 
         return ans;
     }
