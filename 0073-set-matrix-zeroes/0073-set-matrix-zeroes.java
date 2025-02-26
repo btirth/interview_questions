fba9eb7173b87ff63157f2c311ef734a60746853
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-02-25 23:13:30 -0400

    Time: 1 ms (75.47%), Space: 45.8 MB (45.52%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,22 +2,39 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int[] rz = new int[m];
+        boolean topCornerZero = false;
 
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
+                if(j == 0 && matrix[i][j] == 0) {
+                    topCornerZero = true;
+                    continue;
+                }
+
                 if(matrix[i][j] == 0) {
-                    rz[i] = 1;
+                    matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i=m-1; i>=0; i--) {
-            for(int j=0; j<n; j++) {
-                if(rz[i] == 1 || matrix[0][j] == 0) {
+        for(int i=1; i < m; i++) {
+            for(int j=1; j < n; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
+            }
+        }
+
+        if(matrix[0][0] == 0) {
+            for(int i=0; i<n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if(topCornerZero) {
+            for(int i=0; i<m; i++) {
+                matrix[i][0] = 0;
             }
         }
     }
