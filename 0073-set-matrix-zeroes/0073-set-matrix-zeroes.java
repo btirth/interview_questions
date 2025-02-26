ff30abf0f1c4bdba1afcd84ab98b26e57f399205
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-02-25 22:55:15 -0400

    Time: 0 ms (100%), Space: 45.5 MB (82.54%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,20 +3,19 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         int[] rz = new int[m];
-        int[] cz = new int[n];
 
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(matrix[i][j] == 0) {
                     rz[i] = 1;
-                    cz[j] = 1;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i=0; i<m; i++) {
+        for(int i=m-1; i>=0; i--) {
             for(int j=0; j<n; j++) {
-                if(rz[i] == 1 || cz[j] == 1) {
+                if(rz[i] == 1 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
