1025d1e81ace282d944a253efb22f550a4677764
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-05-03 21:05:47 -0300

    Time: 26 ms (22.14%), Space: 56.7 MB (5.85%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.java
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.java
@@ -4,8 +4,7 @@ class Solution {
         int count = 0;
 
         for(int[] d: dominoes) {
-            Arrays.sort(d);
-            String key = d[0] + "-" + d[1];
+            String key = d[0] < d[1] ? d[0] + "-" + d[1] : d[1] + "-" + d[0];
             count += freq.getOrDefault(key, 0);
             freq.put(key, freq.getOrDefault(key, 0) + 1);
         }
