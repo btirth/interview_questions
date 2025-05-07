2c02f356fd09678f3a8cecfe7cb18b1cb533f35c
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-05-07 20:53:02 -0300

    Time: 1 ms (99.99%), Space: 57.5 MB (41.24%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,47 +1,30 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        int n = nums.length;
-        boolean firstFound = false;
-
-        for(int i=0; i<n; i++) {
-            if(nums[i] == 1) {
-                firstFound = true;
-            }
-
-            if(nums[i] < 1) {
-                nums[i] = 1;
+        int len = nums.length;
+        for(int i=0; i<len; i++) {
+            if(nums[i] < 0) {
+                nums[i] = 0;
             }
         }
 
-        if(!firstFound) {
-            return 1;
-        }
-
-        for(int i=0; i<n; i++) {
-            int val = Math.abs(nums[i]);
-            if(val < n) {
-                if(nums[val] < 0) {
-                    continue;
-                }
-
-                nums[val] *= -1; 
+        int i=0;
+        while(i<len) {
+            if(nums[i] != 0 && nums[i] < len && (i+1) != nums[i] && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
             }
         }
 
-        int ans = n;
-        for(int i=2; i<n; i++) {
-            if(nums[i] > 0) {
-                ans = i;
-                break;
+        i = 0;
+        for(; i<len; i++) {
+            if(nums[i] != i+1) {
+                return i + 1;
             }
         }
 
-        for(int i=0; i<n; i++) {
-            if(Math.abs(nums[i]) == ans) {
-                return ans + 1;
-            }
-        }
-
-        return ans;
+        return len + 1;
     }
 }
