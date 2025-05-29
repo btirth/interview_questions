455186040659d8d679a871215bd9e1d83ed1058e
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-05-29 00:36:37 -0300

    Time: 1 ms (18.97%), Space: 42.1 MB (19.96%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -3,42 +3,46 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        if(grid[m-1][n-1] == 1) {
-            return 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] == 1) {
+                    grid[i][j] = -1;
+                }
+            }
         }
 
-        int[][] dp = new int[m][n];
-        dp[m-1][n-1] = 1;
-        for(int i=m-2; i>=0; i--) {
-            if(dp[i+1][n-1] == 0 || grid[i][n-1] == 1) {
-                continue;
+        for(int i=m-1; i>=0; i--) {
+            if(grid[i][n-1] == -1) {
+                break;
             }
-            dp[i][n-1] = 1;
+
+            grid[i][n-1] = 1;
         }
 
-        for(int i=n-2; i>=0; i--) {
-            if(dp[m-1][i+1] == 0 || grid[m-1][i] == 1) {
-                continue;
+        for(int i=n-1; i>=0; i--) {
+            if(grid[m-1][i] == -1) {
+                break;
             }
-            dp[m-1][i] = 1;
+
+            grid[m-1][i] = 1;
         }
 
         for(int i=m-2; i>=0; i--) {
             for(int j=n-2; j>=0; j--) {
-                if(grid[i][j] == 1) {
+                if(grid[i][j] == -1) {
                     continue;
                 }
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+
+                if(grid[i+1][j] != -1) {
+                    grid[i][j] += grid[i+1][j];
+                }
+
+                if(grid[i][j+1] != -1) {
+                    grid[i][j] += grid[i][j+1];
+                }
             }
         }
 
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                System.out.print(dp[i][j] + "-");
-            }
-             System.out.println("");
-        }
-
-        return dp[0][0];
+        return grid[0][0] == -1 ? 0 : grid[0][0];
     }
 }
