677d5adf820acf7cda816085abc4d81db0abf8f5
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-05 21:06:14 -0400

    Time: 1213 ms (7.41%), Space: 45.6 MB (90.03%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,10 +1,71 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> visited = new HashSet<>();
+        /**
+        I can move from word1 to word2 if
+        -> They differs by a single letter
+
+        beginWord not required in the wordList
+        endWord needs to be in the wordList
+
+        Find beginWord -> W1 -> W2 -> ... -> endWord
+
+        max word length = 10
+        wordList length = 5000
+
+        if I visit a word again, I don't want to visit again, because I am looking for shortest trans.
+        visited = []
+
+        We can use Queue, for each iteration over Queue we'll increase the lenght.
+
+        We'll have to create a map, so that we can map each word with possible next word (They differ by single letter)
+        
+        // if n = length of wordList, O(n^2)
+        mapWord(beginWord, wordList) {
+            Map<String, List<String>> map -> Key = word and values = words with a single letter diff
+
+            for word in wordList:
+                List<String> singleLetterDiff
+
+                for nextWord in wordList:
+                    if differBySingleLetter(word, nextWord):
+                        singleLetterDiff.add(nextWord)
+
+                map.put(word, singleLetterDiff) 
+        }
+
+
+        Queue q
+        q.add(beginWord)
+
+        while(q not empty) {
+            int size = q.size();
+
+            while(size-- > 0) {
+                String word = q.poll();
+                if(word === endWOrd) {
+                    return sequence;
+                }
+            }
+
+            sequence++;
+        }
+
+        return 0;
+
+
+        O(n^2) -> to build map
+        TC - O(n)
+        SC - O(n)
+
+         */
+
+        wordList.add(beginWord);
+        HashMap<String, List<String>> map = buildMap(wordList);
+
         Queue<String> q = new LinkedList<>();
-        int len = 0;
-        visited.add(beginWord);
         q.add(beginWord);
+
+        int seq = 0;
 
         while(!q.isEmpty()) {
             int size = q.size();
@@ -12,43 +73,58 @@ class Solution {
             while(size-- > 0) {
                 String word = q.poll();
 
+                // We found the endword so return sequence length
                 if(word.equals(endWord)) {
-                    return len + 1;
-                }
+                    return seq + 1;
+                } 
 
-                for(String next: wordList) {
-                    if(visited.contains(next)) {
-                        continue;
-                    }
-
-                    if(isPossible(word, next)) {
-                        q.add(next);
-                        visited.add(next);
-                    }
+                if(map.containsKey(word)) {
+                    q.addAll(map.get(word));  
+                    map.remove(word);
                 }
             }
 
-            len++;
-        }
+            seq++;
+        } 
 
         return 0;
     }
 
-    boolean isPossible(String s1, String s2) {
-        boolean foundFirstDiffer = false;
+    HashMap<String, List<String>> buildMap(List<String> words) {
+        HashMap<String, List<String>> map = new HashMap<>();
 
-        for(int i=0; i<s1.length(); i++) {
-            if(s1.charAt(i) == s2.charAt(i)) {
-                continue;
+        for(String word: words) {
+            List<String> singleLetterDiff = new ArrayList<>();
+
+            for(String nextWord: words) {
+                if(differBySingleLetter(word, nextWord)) {
+                    singleLetterDiff.add(nextWord);
+                }
             }
 
-            if(foundFirstDiffer) {
-                return false;
-            }
-
-            foundFirstDiffer = true;
+            map.put(word, singleLetterDiff);
         }
 
-        return foundFirstDiffer;
+        return map;
     }
+    
+    boolean differBySingleLetter(String word1, String word2) {
+        if(word1.length() != word2.length()) {
+            return false;
+        }
+
+        boolean found = false;
+        for(int i=0; i<word1.length(); i++) {
+            if(word1.charAt(i) != word2.charAt(i)) {
+                if(found) {
+                    return false;
+                }
+
+                found = true;
+            }
+        }
+
+        return found;
+    }
+
 }
