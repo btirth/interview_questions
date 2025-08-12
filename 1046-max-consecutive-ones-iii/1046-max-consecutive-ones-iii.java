4ee586ec914d1c7886e7423596fd6f1d0a112a88
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-11 20:40:24 -0400

    Time: 4 ms (29.97%), Space: 53.4 MB (6.36%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -1,27 +1,52 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
+        /**
+        max number of consecutive 1's
+
+        can I say res = no. of 1's + no. of 0's < k
+        if use 2 pointers 
+        -> If nums[idx] == 1
+            right++
+           else:
+            while cnt >= k:
+                // move left pointer
+                if nums[left] == 0
+                    cnt--;
+                
+                left++;
+
+           ans = max(ans, right - left + 1)      
+        
+        [1,0,0,0,1,1,1], k = 2
+        [1,0,0] = 3
+        [0,0]
+        []
+    
+         */
+
         int left = 0;
         int right = 0;
-        int longestOnes = 0;
         int len = nums.length;
+        int flipped = 0;
+        int ans = 0;
 
         while(right < len) {
             if(nums[right] == 0) {
-                if(k>0) {
-                    k--;
-                } else {
-                    while(nums[left] != 0 && left < right) {
-                        left++;
+                while(left <= right && flipped >= k) {
+                    if(nums[left] == 0) {
+                        flipped--;
                     }
 
                     left++;
-                }
-            }
+                }        
 
-            longestOnes = Math.max(longestOnes, right - left + 1);
+                flipped++;
+            } 
+
+            ans = Math.max(ans, right - left + 1);
             right++;
         }
 
-        return longestOnes;
+        return ans;
     }
 }
