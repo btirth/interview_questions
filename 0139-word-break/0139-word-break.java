d960dbd93853e82481316e8b99e0a19f8369bf5e
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-13 17:52:18 -0400

    Time: 3 ms (78.27%), Space: 43 MB (66.06%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,17 +1,31 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        int n = s.length();
-        boolean[] dp = new boolean[n + 1];
-        dp[n] = true;
+        /**
+        At each index i, we can check if
+
         
-        for(int i=n-1; i>=0; i--) {
-            for(int j=i+1; j<=n; j++) {
-                if(wordDict.contains(s.substring(i, j)) && dp[j]) {
+        for i in range (0, len):
+            for j in range (i+1, len):
+                if dp[j] && wordDict.contains(s.substring(i, j))
+                    dp[i] = true
+                    found = true
+                    break
+
+        
+         */
+
+        int len = s.length();
+        boolean[] dp = new boolean[len + 1];
+        dp[len] = true;
+
+        for(int i=len-1; i >= 0; i--) {
+            for(int j=i + 1; j<=len; j++) {
+                if(dp[j] && wordDict.contains(s.substring(i, j))) {
                     dp[i] = true;
                     break;
                 }
             }
-        }
+        } 
 
         return dp[0];
     }
