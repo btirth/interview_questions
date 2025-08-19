6a4e91b8795042b73d05799e3497d76627de330c
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-19 18:22:34 -0300

    Time: 15 ms (85.65%), Space: 42.4 MB (69.75%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,40 +1,69 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
+        /**
+        longest palindromic substring
 
-        for(int i=n-1; i>=0; i--) {
-            for(int j=i; j<n; j++) {
-                if(i == j) {
-                    dp[i][j] = true;
-                } else if(i+1 == j) {
-                    if(s.charAt(i) == s.charAt(j)) {
-                        dp[i][j] = true;
-                    }
-                } else {
-                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1]) {
-                        dp[i][j] = true;
-                    }
-                }
+        palindrom -> aba, bab, 
+        
+        odd length -> middle character anything, on both side same values
+        even length -> if we divide the string of equal length one string is reverse of another.
+
+        for character in s:
+            // odd length
+            we'll go idx - 1 and idx + 1, idx is index of character at it is in the center.
+
+
+            // even length
+            There's two option, current index might on left part of the string or right part.
+            baab -> ba|ab , we'll only check index part of left part of the palindrom.
+            if s[idx] == s[idx + 1]:
+
+        TC -> O(n^2) 
+        SC -> O(1)
+
+
+            b a b   
+        b   1 0 3
+        a     1 0
+        b       1
+
+        TC -> O(n^2)
+        SC -> O(n^2)
+         */
+
+        String res = "";
+        int start = 0;
+        int end = 0;
+        int resLen = 1;
+        int length = s.length();
+
+        for(int i=0; i<length; i++) {
+            // Odd length
+            int j = 1;
+
+            while((i-j) >= 0 && (i+j) < length && s.charAt(i - j) == s.charAt(i + j)) {
+                j++;
             }
-        }
 
-        int[] ans = new int[2];
-        int ansLen = 0;
-
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(dp[i][j]) {
-                    int len = j - i;
-
-                    if(len > ansLen) {
-                        ans = new int[]{i,j};
-                        ansLen = len;
-                    }
-                }
+            if(resLen < (2 * (j - 1) + 1)) {
+                start = i - (j-1);
+                end = i + (j-1);
+                resLen = (2 * (j - 1) + 1);
             }
-        }
 
-        return s.substring(ans[0], ans[1]+1);
+            // Even length
+            j = 0;
+            while((i-j) >= 0 && (i+j+1) < length && s.charAt(i-j) == s.charAt(i+j+1)) {
+                j++;
+            }
+
+            if(resLen < (2 * j)) {
+                start = i - (j-1);
+                end = i + j;
+                resLen = 2 * j;
+            }
+        } 
+
+        return s.substring(start, end + 1);
     }
 }
