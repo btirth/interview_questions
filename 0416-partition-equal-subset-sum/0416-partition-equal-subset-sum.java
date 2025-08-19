55eec03cd99921b37cb92e62f7845364ef66fe9f
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-19 10:34:47 -0300

    Time: 15 ms (97.65%), Space: 42 MB (93.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,47 +1,37 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0;
+        int n = nums.length;
+        int totalSum = 0;
+
         for(int num: nums) {
-            sum += num;
+            totalSum += num;
         }
 
-        if(sum%2 == 1) {
+        if(totalSum % 2 != 0) {
             return false;
         }
 
-        return partition(nums, sum/2);
+        int target = totalSum / 2;
+        return helper(nums, target);
     }
 
-    boolean partition(int[] nums, int target) {
+    boolean helper(int[] nums, int target) {
         int n = nums.length;
         boolean[] dp = new boolean[target + 1];
+        dp[0] = true;
 
-        for(int i=0; i<n; i++) {
-            boolean[] newDp = new boolean[target + 1];
-            if(nums[i] > target) {
-                return false;
-            }
-            newDp[nums[i]] = true;
+        for(int num: nums) {
+            for(int i=target; i >= num; i--) {
+                if(!dp[i] && dp[i - num]) {
+                    dp[i] = true;
 
-            for(int j=1; j<=target; j++) {
-                if(dp[j]) {
-                    newDp[j] = true;
-                }
-
-                if(dp[j] && j + nums[i] <= target) {
-                    newDp[j + nums[i]] = true;
-
-                    if(j + nums[i] == target) {
+                    if(i == target) {
                         return true;
                     }
                 }
             }
-
-            dp = newDp;
-
-
         }
 
-        return dp[target];
+        return false;
     }
 }
