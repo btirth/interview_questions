5d96256c726bdbd28325c89f6024c2dd0539d5d0
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-19 18:58:59 -0300

    Time: 0 ms (100%), Space: 41.7 MB (87.52%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,51 +1,49 @@
 class Solution {
+    int[] dp;
     public int numDecodings(String s) {
-        int n = s.length();
-        int[] dp = new int[n + 1];
-        dp[n] = 1;
+        /**
+        11106
 
-        for(int i=n-1; i>=0; i--) {
-            if(s.charAt(i) == '0') {
-                dp[i] = 0;
-                continue;
-            }
+        Can I say -> 1 + findAns(1106)
+        1 + 1 + findAns(106)
+        11 + findAns(106)
+        
+        
+         */
+        int length = s.length();
+        dp = new int[length];
+        Arrays.fill(dp, -1);
+        return decodeString(s, 0);
+    }
 
-            dp[i] += dp[i + 1];
+    int decodeString(String s, int idx) {
+        int ways = 0;
+        int code = 0;
 
-             if(i < s.length() - 1 && 
-             (s.charAt(i) == '1' || (s.charAt(i) == '2' && s.charAt(i+1) < '7'))) {
-           
-                dp[i] += dp[i + 2];
-         
+        if(idx == s.length()) {
+            return 1;
+        }
+
+        if(s.charAt(idx) == '0') {
+            // code cann't start with 0.
+            return dp[idx] = 0;
+        }
+        
+        if(dp[idx] != -1) {
+            return dp[idx];
+        }
+
+        for(int i=idx; i<s.length(); i++) {
+            int currDigit = s.charAt(i) - '0';
+            code = (code * 10) + currDigit;
+
+            if(code >= 1 && code <= 26) {
+                ways += decodeString(s, i + 1);
+            } else {
+                break;
             }
         }
 
-        return dp[0];
+        return dp[idx] = ways;
     }
-
-    // int helper(String s, int idx) {
-    //     if(s.length() == idx) {
-    //         return 1;
-    //     }
-
-    //     if(s.charAt(idx) == '0') {
-    //         return 0;
-    //     }
-
-    //     if(dp[idx] != -1) {
-    //         return dp[idx];
-    //     }
-
-    //     int val = 0;
-    //     val += helper(s, idx + 1);
-
-    //     if(idx < s.length() - 1 && (s.charAt(idx) == '1' || (s.charAt(idx) == '2' && s.charAt(idx+1) < '7'))) {
-           
-    //             val += helper(s, idx + 2);
-         
-    //     }
-
-    //     return dp[idx] = val;
-    // }
-
 }
