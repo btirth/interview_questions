92cbcd612b3e7c403a7a7de09a48c64fd6d5369b
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-19 23:28:19 -0300

    Time: 14 ms (20.18%), Space: 49.1 MB (42.81%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    Integer[][] dp;
+    int[][] dp;
     public int change(int amount, int[] coins) {
         /**
         for coin in coins:
@@ -7,24 +7,39 @@ class Solution {
             ways += change(amount - coin, coins, idx + 1);
          */
         int totalCoins = coins.length; 
-        dp = new Integer[amount + 1][totalCoins];
-        return waysOfChange(amount, coins, 0);
-    }
+        dp = new int[totalCoins][amount + 1];
 
-    int waysOfChange(int amount, int[] coins, int idx) {
-        if(amount == 0) {
-            return 1;
-        } else if(amount < 0 || idx >= coins.length) {
-            return 0;
+        dp[0][0] = 1;        
+        for(int c=0; c<totalCoins; c++) {
+            int coin = coins[c];
+
+            for(int i=0; i <= amount; i++) {
+                if(i>=coin) {
+                    dp[c][i] += dp[c][i - coin];
+                }
+               
+                if(c>0)
+                    dp[c][i] += dp[c-1][i];
+            }
         }
 
-        if(dp[amount][idx] != null) {
-            return dp[amount][idx];
-        }
-
-        int ways = waysOfChange(amount - coins[idx], coins, idx);
-        ways += waysOfChange(amount, coins, idx + 1);
-
-        return dp[amount][idx] = ways;
+        return dp[totalCoins-1][amount];
     }
+
+    // int waysOfChange(int amount, int[] coins, int idx) {
+    //     if(amount == 0) {
+    //         return 1;
+    //     } else if(amount < 0 || idx >= coins.length) {
+    //         return 0;
+    //     }
+
+    //     if(dp[amount][idx] != null) {
+    //         return dp[amount][idx];
+    //     }
+
+    //     int ways = waysOfChange(amount - coins[idx], coins, idx);
+    //     ways += waysOfChange(amount, coins, idx + 1);
+
+    //     return dp[amount][idx] = ways;
+    // }
 }
