b90a2b86c8c4a82d72a4c95fd1b52b24cc57d509
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-22 14:50:20 -0300

    Time: 48 ms (5.84%), Space: 68.2 MB (5.08%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,37 +1,50 @@
 class Solution {
-    Boolean[][] dp;
+    Boolean[][][] dp;
     public boolean isInterleave(String s1, String s2, String s3) {
+        /**
+        helper(s1, s2, s3, i1, i2, i3) {
+            if(s3[i3] == s1[i1]) {
+
+            }
+        }
+        
+        
+         */
+
         int len1 = s1.length();
         int len2 = s2.length();
         int len3 = s3.length();
 
-        if(len1 + len2 != len3) {
+        if(len3 != (len1 + len2)) {
             return false;
         }
 
-        dp = new Boolean[101][101];
+        dp = new Boolean[101][101][201];
 
-        return helper(s1, s2, s3, 0, 0);
+        return isInterleave(s1, s2, s3, 0, 0, 0);
     }
 
-
-    boolean helper(String s1, String s2, String s3, int idx1, int idx2) {
-        if(idx1 + idx2 == s3.length()) {
+    boolean isInterleave(String s1, String s2, String s3, int idx1, int idx2, int idx3) {
+        if(idx3 == s3.length()) {
             return true;
         }
 
-        if(dp[idx1][idx2] != null) {
-            return dp[idx1][idx2];
+        if(dp[idx1][idx2][idx3] != null) {
+            return dp[idx1][idx2][idx3];
         }
 
-        if(idx1 < s1.length() && s1.charAt(idx1) == s3.charAt(idx1 + idx2) && helper(s1, s2, s3, idx1+1, idx2)) {
-            return dp[idx1][idx2] = true;
+        if(idx1 < s1.length() && s1.charAt(idx1) == s3.charAt(idx3)) {
+            if(isInterleave(s1, s2, s3, idx1 + 1, idx2, idx3 + 1)) {
+                return dp[idx1][idx2][idx3] = true;
+            }
         }
 
-        if(idx2 < s2.length() && s2.charAt(idx2) == s3.charAt(idx1 + idx2) && helper(s1, s2, s3, idx1, idx2+1)) {
-            return dp[idx1][idx2] = true;
+        if(idx2 < s2.length() && s2.charAt(idx2) == s3.charAt(idx3)) {
+            if(isInterleave(s1, s2, s3, idx1, idx2 + 1, idx3 + 1)) {
+                return dp[idx1][idx2][idx3] = true;
+            }
         }
 
-        return dp[idx1][idx2] = false;
+        return dp[idx1][idx2][idx3] = false;
     }
 }
