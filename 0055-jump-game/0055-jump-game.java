18bab42ccf6037ed34a151b6193986675c163dec
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-22 22:10:50 -0300

    Time: 322 ms (8.54%), Space: 46 MB (31.34%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,24 +1,27 @@
 class Solution {
+    Boolean[] dp;
     public boolean canJump(int[] nums) {
-        /**
-        [2,3,1,1,4]
-
-        4 - true
-        1 - true
-        1 - true
-        3 - true
-        2 - true
+        int len = nums.length;
+        dp = new Boolean[len];
         
-         */
+        return canJump(nums, 0);
+    }
 
-        int n = nums.length;
-        int lastGoodIdx = n - 1;
-        for(int i=n-2; i>=0; i--) {
-            if(lastGoodIdx - i <= nums[i]) {
-                lastGoodIdx = i;
+    boolean canJump(int[] nums, int idx) {
+        if(idx == nums.length - 1) {
+            return true;
+        }
+
+        if(dp[idx] != null) {
+            return dp[idx];
+        }
+
+        for(int i=1; i<=nums[idx]; i++) {
+            if(canJump(nums, idx + i)) {
+                return dp[idx] = true;
             }
-        } 
+        }
 
-        return lastGoodIdx == 0;
+        return dp[idx] = false;
     }
 }
