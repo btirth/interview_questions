978bd0543143add3f8b9009999856e17c5007d02
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-23 15:09:17 -0300

    Time: 1 ms (99.54%), Space: 57 MB (56.01%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
+        /**
+        If sum goes < 0 -> forget about sum
+        If sum < num -> isn't it better to just take num as new sum?
+        
+         */
+        int maxSum = nums[0];
         int sum = 0;
-        int ans = Integer.MIN_VALUE;
 
         for(int num: nums) {
-            sum += num;
-            ans = Math.max(ans, sum);
-            if(sum < 0) {
-                sum = 0;
-            }
+            sum = Math.max(num, sum + num);
+            maxSum = Math.max(maxSum, sum);
         }
 
-        return ans;
+        return maxSum;
     }
 }
