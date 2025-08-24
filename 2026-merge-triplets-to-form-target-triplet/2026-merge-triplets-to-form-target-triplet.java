289543f7bc1f637ddbd66ca542f0edd1640e46bc
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-08-24 16:04:41 -0300

    Time: 1 ms (99.51%), Space: 119.7 MB (69.41%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.java
@@ -1,21 +1,36 @@
 class Solution {
     public boolean mergeTriplets(int[][] triplets, int[] target) {
-        boolean[] possible = new boolean[3];
-
-        for(int[] triplet: triplets) {
-            if(triplet[0] <= target[0] && 
-                triplet[1] <= target[1] && 
-                triplet[2] <= target[2]
-            ) {
-                possible[0] = possible[0] | triplet[0] == target[0];
-                possible[1] = possible[1] | triplet[1] == target[1];
-                possible[2] = possible[2] | triplet[2] == target[2];
+        /**
+        I will pick every triplets for which a[0] < t[0] , a[1] < t[1] and a[2] < t[2]
+        
+         */
+        
+        int len = triplets.length;
+        boolean foundFirst = false;
+        boolean foundSecond = false;
+        boolean foundThird = false; 
+        
+        for(int i=0; i<len; i++) {
+            if(triplets[i][0] > target[0] || triplets[i][1] > target[1] || triplets[i][2] > target[2]) {
+                continue;
             }
 
-            if(possible[0] && possible[1] && possible[2]) {
+            if(triplets[i][0] == target[0]) {
+                foundFirst = true;
+            }
+
+            if(triplets[i][1] == target[1]) {
+                foundSecond = true;
+            }
+
+            if(triplets[i][2] == target[2]) {
+                foundThird = true;
+            }
+
+            if(foundFirst && foundSecond && foundThird) {
                 return true;
             }
-        }
+        } 
 
         return false;
     }
