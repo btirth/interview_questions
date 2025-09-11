8e71f6d85eaa4edb7964c1b72187aa7455172f22
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2025-09-11 12:08:26 -0300

    Time: 65 ms (26.15%), Space: 46.5 MB (61.74%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.java
@@ -1,35 +1,44 @@
 class Solution {
-    // Returns true if the character is a vowel.
-    boolean isVowel(Character c) {
-        return c == 'a' || c == 'e' || c == 'o'|| c == 'u'|| c == 'i' 
-            || c == 'A' || c == 'E' || c == 'O'|| c == 'U'|| c == 'I';
-    }
-    
     public String sortVowels(String s) {
-         ArrayList<Character> temp = new ArrayList<>();
+        HashMap<Character, Integer> freq = new HashMap<>();
+        List<Character> vowels = new ArrayList<>();    
+        vowels.add('A');
+        vowels.add('E');
+        vowels.add('I');
+        vowels.add('O');
+        vowels.add('U');
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
 
-        // Store the vowels in the temporary string.
-        for (char c : s.toCharArray()) {
-            if (isVowel(c)) {
-                temp.add(c);
+
+
+        for(char ch: s.toCharArray()) {
+            if(vowels.contains(ch))
+                freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        }
+
+        char[] res = s.toCharArray();
+        int vowelIdx = 0;
+
+        for(int i=0; i<s.length(); i++) {
+            if(vowels.contains(res[i])) {
+                while(vowelIdx < vowels.size() && !freq.containsKey(vowels.get(vowelIdx))) {
+                    vowelIdx++;
+                }
+
+                char ch = vowels.get(vowelIdx);
+                res[i] = ch;
+                freq.put(ch, freq.get(ch) - 1);
+
+                if(freq.get(ch) == 0) {
+                    freq.remove(ch);
+                }
             }
         }
-        
-        // Sort the temporary string characters in ascending order.
-        Collections.sort(temp);
 
-        StringBuilder ans = new StringBuilder();
-        int j = 0;
-        for (int i = 0; i < s.length(); i++) {
-            // If the character is a vowel, replace it with the character in the string temp.
-            if (isVowel(s.charAt(i))) {
-                ans.append(temp.get(j));
-                j++;
-            } else {
-                ans.append(s.charAt(i));
-            }
-        }
-        
-        return ans.toString();
+        return new String(res);
     }
-};
+}
