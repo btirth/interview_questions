9bb9423a1fbb7df39c1748a2743eef954f964082
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2026-03-29 18:54:59 -0400

    Time: 4 ms (99.83%), Space: 43.9 MB (90.81%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
+        int[] freq = new int[26];
 
         for(char ch: s1.toCharArray()) {
-            freq1[ch - 'a']++;
+            freq[ch - 'a']++;
         }
 
         int left = 0;
@@ -14,10 +13,10 @@ class Solution {
 
         while(right < n2) {
             char ch = s2.charAt(right);
-            freq2[ch - 'a']++;
+            freq[ch - 'a']--;
             
-            while(freq2[ch - 'a'] > freq1[ch - 'a']) {
-                freq2[s2.charAt(left) - 'a']--;
+            while(freq[ch - 'a'] < 0) {
+                freq[s2.charAt(left) - 'a']++;
                 left++;
             }
             
