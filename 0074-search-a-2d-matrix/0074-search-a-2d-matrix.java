2ccf503926be93abb051c035fbf4b2cce7096e34
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2026-03-28 21:26:41 -0400

    Time: 0 ms (100%), Space: 44.1 MB (34.2%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,44 +1,33 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = bsRow(matrix, target);
+        int m = matrix.length;
         int n = matrix[0].length;
-        if(row < 0 || row >= matrix.length) {
-            return false;
-        }
-
         int i = 0;
-        int j = n-1;
+        int j = (m*n) - 1;
 
-        while(i <= j) {
-            int mid = (j+i) / 2;
-            if(matrix[row][mid] == target) {
+        /**
+        m = 3
+        n = 4
+        i = 0, j = 11
+        mid = 11/2 = 5
+        r = ceil(5/n) - 1 = 1
+        c = mid - (r * n) = 5 - (1*4) = 1
+        
+         */
+        while(i<=j) {
+            int mid = i + (j-i) / 2;
+            int r = mid / n;
+            int c = mid % n;
+
+            if(matrix[r][c] == target) {
                 return true;
-            } else if(matrix[row][mid] < target) {
-                i = mid + 1;
-            } else {
+            } else if(matrix[r][c] > target) {
                 j = mid - 1;
+            } else {
+                i = mid + 1;
             }
         }
 
         return false;
-    }
-
-    int bsRow(int[][] matrix, int target) {
-        int i = 0;
-        int j = matrix.length - 1;
-        int n = matrix[0].length;
-
-        while(i <= j) {
-            int mid = (j + i) / 2;
-            if(matrix[mid][0] <= target && matrix[mid][n-1] >= target) {
-                return mid;
-            } else if(matrix[mid][0] < target) {
-                i = mid + 1;
-            } else {
-                j = mid - 1;
-            }
-        }
-
-        return -1;
     }
 }
