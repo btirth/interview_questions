59ef76aab7af54629b93652545d4095647b05cc7
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2026-03-28 20:49:27 -0400

    Time: 0 ms (100%), Space: 44 MB (53.77%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,51 +1,41 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = binarySearchRow(matrix, target);
-        if(row == -1) {
+        int row = bsRow(matrix, target);
+        int n = matrix[0].length;
+        if(row < 0 || row >= matrix.length) {
             return false;
         }
 
-        int col = binarySearchCol(matrix, row, target);
+        int i = 0;
+        int j = n-1;
 
-        if(col == -1) {
-            return false;
-        }
-
-        return true;
-    }
-
-    int binarySearchRow(int[][] matrix, int target) {
-        int up = 0;
-        int down = matrix.length - 1;
-        int n = matrix[0].length - 1;
-
-        while(up <= down) {
-            int mid = (up + down) / 2;
-            if(matrix[mid][n] >= target && (mid == 0 || matrix[mid - 1][n] < target)) {
-                return mid;
-            } else if(matrix[mid][n] > target) {
-                down = mid - 1;
+        while(i <= j) {
+            int mid = (j+i) / 2;
+            if(matrix[row][mid] == target) {
+                return true;
+            } else if(matrix[row][mid] < target) {
+                i = mid + 1;
             } else {
-                up = mid + 1;
+                j = mid - 1;
             }
         }
 
-        return -1;
+        return false;
     }
 
-    int binarySearchCol(int[][] matrix, int row, int target) {
-    
-        int left = 0;
-        int right = matrix[0].length - 1;
-    
-        while(left <= right) {
-            int mid = (right + left) / 2;
-            if(matrix[row][mid] == target) {
+    int bsRow(int[][] matrix, int target) {
+        int i = 0;
+        int j = matrix.length - 1;
+        int n = matrix[0].length;
+
+        while(i <= j) {
+            int mid = (j + i) / 2;
+            if(matrix[mid][0] <= target && matrix[mid][n-1] >= target) {
                 return mid;
-            } else if(matrix[row][mid] > target) {
-                right = mid - 1;
+            } else if(matrix[mid][0] < target) {
+                i = mid + 1;
             } else {
-                left = mid + 1;
+                j = mid - 1;
             }
         }
 
