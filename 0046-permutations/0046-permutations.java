0105e3cd2356279c68c4f6ee22b80f22dbc5ee76
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2026-04-07 18:40:18 -0400

    Time: 2 ms (37.27%), Space: 45.6 MB (53.8%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,40 +1,21 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        /**
-        [1,2,3]
-        [2,1,3]
-        [3,2,1]
-
-        [1,3,2]
-        [2,3,1]
-        [3,1,2]
-
-         */
-        int n = nums.length;
-        List<List<Integer>> ans = new ArrayList<>(); 
-        List<Integer> firstList = new ArrayList<>();
-        for(int num: nums) {
-            firstList.add(num);
-        }
-        ans.add(firstList);
-        for(int i=0; i<n; i++) {
-            List<List<Integer>> newAns = new ArrayList<>(); 
-            for(int j=i+1; j<n; j++) {
-                for(List<Integer> list: ans) {
-                    List<Integer> newList = new ArrayList<>(list);
-                    int temp1 = newList.get(i);
-                    int temp2 = newList.get(j);
-
-                    newList.set(i, temp2);
-                    newList.set(j, temp1);
-
-                    newAns.add(newList);
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for(int idx = 0; idx < nums.length; idx++) {
+            List<List<Integer>> nextRes = new ArrayList<>();
+            for(List<Integer> curr: res) {
+                
+                for(int i=0; i<=curr.size(); i++) {
+                    List<Integer> next = new ArrayList<>(curr);
+                    next.add(i, nums[idx]);
+                    nextRes.add(next);
                 }
             }
 
-            ans.addAll(newAns);
+            res = nextRes;
         }
 
-        return ans;
+        return res;
     }
 }
