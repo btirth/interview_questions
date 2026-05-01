c2ba0d00e55eacd951ad6960832c4964ea48c7da
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2026-05-01 18:34:41 -0400

    Time: 13 ms (95.98%), Space: 46.4 MB (40.92%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,68 +1,30 @@
 class Solution {
+    String res = "";
     public String longestPalindrome(String s) {
-        /**
-        longest palindromic substring
-
-        palindrom -> aba, bab, 
+        int n = s.length();
         
-        odd length -> middle character anything, on both side same values
-        even length -> if we divide the string of equal length one string is reverse of another.
-
-        for character in s:
-            // odd length
-            we'll go idx - 1 and idx + 1, idx is index of character at it is in the center.
-
-
-            // even length
-            There's two option, current index might on left part of the string or right part.
-            baab -> ba|ab , we'll only check index part of left part of the palindrom.
-            if s[idx] == s[idx + 1]:
-
-        TC -> O(n^2) 
-        SC -> O(1)
-
-
-            b a b   
-        b   1 0 3
-        a     1 0
-        b       1
-
-        TC -> O(n^2)
-        SC -> O(n^2)
-         */
-
-        int start = 0;
-        int end = 0;
-        int resLen = 1;
-        int length = s.length();
-
-        for(int i=0; i<length; i++) {
+        for(int i=0; i<n; i++) {
             // Odd length
-            int j = 1;
-
-            while((i-j) >= 0 && (i+j) < length && s.charAt(i - j) == s.charAt(i + j)) {
-                j++;
-            }
-
-            if(resLen < (2 * (j - 1) + 1)) {
-                start = i - (j-1);
-                end = i + (j-1);
-                resLen = (2 * (j - 1) + 1);
-            }
+            checkPalindrom(s, i-1, i+1);
 
             // Even length
-            j = 0;
-            while((i-j) >= 0 && (i+j+1) < length && s.charAt(i-j) == s.charAt(i+j+1)) {
-                j++;
-            }
+            checkPalindrom(s, i, i+1);
+        }
 
-            if(resLen < (2 * j)) {
-                start = i - (j-1);
-                end = i + j;
-                resLen = 2 * j;
-            }
-        } 
+        return res;
+    }
 
-        return s.substring(start, end + 1);
+    void checkPalindrom(String s, int l, int r) {
+        int n = s.length();
+
+        while(l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+
+        int length = (r - l - 1);
+        if(length > res.length()) {
+            res = s.substring(l+1, r);
+        }
     }
 }
