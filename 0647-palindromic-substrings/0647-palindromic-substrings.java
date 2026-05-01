05d4ac3b62adb05372dd5f81e912bd515c39cd39
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2026-05-01 18:47:17 -0400

    Time: 5 ms (97.68%), Space: 43.1 MB (42.35%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,25 +1,32 @@
 class Solution {
     public int countSubstrings(String s) {
-        int length = s.length();
-        int count = length;
+        int n = s.length();
+        int count = 0;
 
-        for(int i=0; i<length; i++) {
+        for(int i=0; i<n; i++) {
             // Odd length
-            int j = 1;
-
-            while((i-j) >= 0 && (i+j) < length && s.charAt(i - j) == s.charAt(i + j)) {
-                j++;
-                count++;
-            }
+            count += countPalindrom(s, i-1, i+1);
 
             // Even length
-            j = 0;
-            while((i-j) >= 0 && (i+j+1) < length && s.charAt(i-j) == s.charAt(i+j+1)) {
-                j++;
-                count++;
-            }
-        } 
+            count += countPalindrom(s, i, i+1);
+        }
 
         return count;
+    }
+
+    int countPalindrom(String s, int l, int r) {
+        int n = s.length();
+
+        while(l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+
+        int length = r - l - 1;
+        if(length%2 == 0) {
+            return length / 2;
+        }
+
+        return (length + 1) / 2;
     }
 }
