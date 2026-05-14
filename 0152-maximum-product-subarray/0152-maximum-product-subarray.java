bf6f6b52c043ae6033702ba3f3dd09331ab6ab11
Tirth Bharatiya <41485330+btirth@users.noreply.github.com>
2026-05-13 20:49:23 -0400

    Time: 3 ms (19.79%), Space: 47 MB (93.61%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,25 +1,23 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        /**
-        min = 0, max = 0
-        2 -> min = 2, max = 2
-        3 -> min = 1, max = 6
-        -2 -> min = -2, max = -2
-
-        max = max(num, max(min * num, max * num));
-        min = min(num, min(min * num, max * num));
-        
-         */
-        int minProd = nums[0];
-        int maxProd = nums[0];
+        int max = 1;
+        int min = 1;
         int ans = nums[0];
 
-        for(int i=1; i<nums.length; i++) {
-            int num = nums[i];
-            int tempMinProd = minProd;
-            minProd = Math.min(num, Math.min(minProd * num, maxProd * num));
-            maxProd = Math.max(num, Math.max(tempMinProd * num, maxProd * num));
-            ans = Math.max(ans, Math.max(minProd, maxProd));
+        for(int num: nums) {
+            int tempMax = max;
+            
+            max = Math.max(max * num, min * num);
+            max = Math.max(max, num);
+            min = Math.min(min * num, tempMax * num);
+            min = Math.min(min, num);
+
+            ans = Math.max(ans, Math.max(max, min));
+
+            if(num == 0) {
+                max = 1;
+                min = 1;
+            }
         }
 
         return ans;
